682d4cc7d49640733088573131096fd4593b7f72
Craig Barnes <craigrbarnes@uchicago.edu>
2022-01-07 10:34:57 -0600

    add next config file

---
+++ packages/portal/next.config.js
@@ -0,0 +1,27 @@
+const withPlugins = require('next-compose-plugins');
+const withMDX = require('@next/mdx')({
+    extension: /\.(md|mdx)$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+})
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer:
+                { and: [/\.(js|ts)x?$/] },
+            use: ['@svgr/webpack'],
+        });
+
+        return config
+    },
+};
+
+module.exports = withPlugins([
+    [withMDX, { pageExtensions: ["mdx", "tsx"] }],
+
+],nextConfig );
