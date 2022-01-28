e86ccfb7d8eb8f0b4fbb077c512bf3f8ffcf281d
Matthew Cannalte <mcannalte@uchicago.edu>
2022-01-28 17:03:47 -0600

    feat(heal-landing-deployment): image paths

--- packages/portal/src/components/Navigation/Footer.tsx
+++ packages/portal/src/components/Navigation/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Footer: React.FC<unknown> = () => {
+    const { basePath } = useRouter();
+
     return (
         <div className="flex flex-row bg-coal justify-end align-middle p-4 h-24 gap-x-8">
-            <Image className="mr-20" src="/icons/gen3.png" layout="fixed" width="120px" height="60px" />
-            <Image src="/icons/createdby.png" layout="fixed" width="180px" height="60px" />
+            <Image className="mr-20" src={`${basePath}/icons/gen3.png`}  layout="fixed" width="120px" height="60px" />
+            <Image src={`${basePath}/icons/createdby.png`} layout="fixed" width="180px" height="60px" />
         </div>
     );
 };
