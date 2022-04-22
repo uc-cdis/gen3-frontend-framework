f608fa207d5124c36516125f8d1d977d19ff02ee
ocshawn <shawnoconnor@uchicago.edu>
2022-04-22 14:29:41 -0500

    feat/landingPage: convert landing page to use config file and also made updates

--- packages/portal/src/components/Contents/LandingPageContent.tsx
+++ packages/portal/src/components/Contents/LandingPageContent.tsx
@@ -1,74 +1,135 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from "next/link";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-import { Title } from '@mantine/core';
+import { Title, TitleOrder } from '@mantine/core';
 
+import { IconType } from 'react-icons';
 import { MdFormatQuote, MdOutlineSearch, MdOutlineBarChart, MdGroup } from 'react-icons/md';
 import { FaExternalLinkAlt, FaGraduationCap } from 'react-icons/fa';
 
-
-import { RoleContentEntry } from "./RolesPageContent";
-
 export interface LandingPageContentProp {
-    rolesPages: Record<string, RoleContentEntry>
+    content: LandingPageProps
 }
 
-const LandingPageContent = ({ rolesPages }: LandingPageContentProp) => {
-    const { basePath } = useRouter();
 
+export interface leftRightProps {
+    readonly text?: string;
+    readonly link?: {
+        readonly href: string;
+        readonly text: string;
+    };
+    readonly image?: {
+        readonly src: string;
+        readonly alt: string;
+    };
+}
+export interface LandingPageProps {
+    readonly topTitle?: string;
+    readonly body?: ReadonlyArray<{
+        readonly title?: {
+            readonly text: string;
+            readonly level: TitleOrder;
+        };
+        readonly splitarea?: {
+            readonly left: leftRightProps[];
+            readonly right: leftRightProps[];
+        };
+        readonly break?: string;
+        readonly cardsArea?: {
+            readonly title: string;
+            readonly cards: ReadonlyArray<{
+                readonly icon: keyof IconType;
+                readonly bodyText: string;
+                readonly btnText: string;
+                readonly link: string;
+            }>;
+        };
+        readonly quoteArea?: {
+            readonly quote: string;
+            readonly author: string;
+        };
+    }>;
+}
+
+const LandingPageContent = ({ content }: LandingPageContentProp) => {
+    const { basePath } = useRouter();
     return (
         <div className='sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-xl my-10 text-heal-dark_gray'>
-            <Title  className='mx-20' order={2}>HEAL Platform</Title>
-            <div className='flex mx-20'>
-                <div className='basis-1/2 pr-10'>
-                    <div className=''>
-                        <p className='!mt-0'>
-                            The <strong>HEAL Platform</strong> is a cloud-based web interface that provides a secure environment for discovery and analysis of NIH HEAL <strong>results and data.</strong>
-                        </p>
-                        <a className='heal-btn' href='/discovery'>Explore HEAL Data</a>
+            {content?.body?.map((component, index)=>{
+                if (component.title) {
+                    return <Title key={index} className='mx-20' order={component.title.level}>{component.title.text}</Title>;
+                }
+                if (component.splitarea) {
+                    const splitareaJsx = (area:leftRightProps[]) => area.map((obj, index)=>{
+                        if (obj.text) {
+                            return <p key={index} className='!mt-0' dangerouslySetInnerHTML={{__html: obj.text}}/>;
+                        }
+                        if (obj.link) {
+                            // if external link
+                            if (obj.link.href.indexOf('//') > -1) {
+                                return <a key={index} className='heal-btn mb-5' href={obj.link.href} target='_blank' rel="noreferrer">                            <FaExternalLinkAlt className='inline-block pb-1 pr-1' title='External Link'/> {obj.link.text}</a>
+                            }
+                            return <Link key={index} href={obj.link.href}>
+                                <a className='heal-btn mb-5'>
+                                    {obj.link.text}
+                                </a>
+                            </Link>;
+                        }
+                        if (obj.image) {
+                            return <Image
+                                key={index}
+                                src={`${basePath}${obj.image.src}`}
+                                alt={obj.image.alt}
+                                width={1260}
+                                height={630}
+                                layout='intrinsic'
+                            />;
+                        }
+                    });
+                    return <div key={index} className='flex mx-20'>
+                        <div className='basis-1/2 pr-10'>
+                            {splitareaJsx(component.splitarea.left)}
+                        </div>
+                        <div className='basis-1/2'>
+                            {splitareaJsx(component.splitarea.right)}
+                        </div>
+                    </div>;
+                }
+                if (component.break) {
+                    return <hr key={index} className='border'/>
+                }
+                if (component.cardsArea) {
+                    const allowedIcons = {
+                        FaGraduationCap: FaGraduationCap,
+                        MdOutlineSearch: MdOutlineSearch,
+                        MdOutlineBarChart: MdOutlineBarChart,
+                        MdGroup: MdGroup,
+                    }
+                    return <div key={index} className='text-center'>
+                        <Title  className='' order={3}>{component.cardsArea.title}</Title>
+                        <ul className='gap-4 mx-20 !p-0'>
+                            {component.cardsArea.cards.map((card, index) => (
+                            <li key={index} className='border shadow-lg !p-5 w-1/5 inline-block mx-5 align-top'>
+                                {React.createElement(allowedIcons[card.icon], {title:`${card.btnText} icon`, className:'inline-block text-7xl text-heal-magenta'}) }
+                                <p className='block text-gen3-titanium leading-6 h-20'>{card.bodyText}</p>
+                                <Link href={`${card.link}`}><a className='heal-btn heal-btn-rev'>{card.btnText}</a></Link>
+                            </li>
+                            ))}
+                        </ul>
+                    </div>;
+                }
+                if (component.quoteArea) {
+                    return <div key={index} className='bg-heal-light_purple p-20 text-center mt-20'>
+                        <div className='text-4xl'><MdFormatQuote title='quotation mark' className='inline rotate-180 text-5xl mb-2'/>{component.quoteArea.quote}<MdFormatQuote title='quotation mark' className='inline text-5xl mt-2'/></div>
+                        <div>{component.quoteArea.author}</div>
                     </div>
-                    <div className=''>
-                        <p>
-                            The Helping to End Addiction Long-term Initiative, or <strong>NIH HEAL Initiative</strong>, is an aggressive, trans-agency effort to speed scientific solutions to stem the national opioid and pain public health crisis.
-                        </p>
-                        <a className='heal-btn' href='//heal.nih.gov' target='_blank' rel="noreferrer">                            <FaExternalLinkAlt className='inline-block pb-1 pr-1' title='External Link'/> Learn More</a>
-                    </div>
-                </div>
-                <div className='basis-1/2'>
-                    <Image
-                        src={`${basePath}/images/HEAL_Initiative.jpeg`}
-                        alt="HEAL initiative"
-                        width={1260}
-                        height={630}
-                        layout='intrinsic'
-                    />
-                </div>
-            </div>
-            <hr className='border'/>
-            <div className='text-center'>
-                <Title  className='' order={3}>What can you expect to do on the HEAL Platform?</Title>
-                <ul className='gap-4 mx-20 !p-0'>
-                {[
-                    [MdOutlineSearch, 'Search datasets from HEAL research.', 'Discover', '/discovery'],
-                    [MdOutlineBarChart, 'Export and analyze datasets on the fly.', 'Explore Tutorial and Example Analysis', '/example'],
-                    [MdGroup, 'Explore how to get involved with the community.', 'Community', '/community'],
-                    [FaGraduationCap, 'Learn about new findings in HEAL research.', 'Learn', '/learn'],
-                ].map(([icon, bodyText, btnText, link], index) => (
-                    <li key={index} className='border shadow-lg !p-5 w-1/5 inline-block mx-5 align-top'>
-                        {React.createElement(icon, {title:`${btnText} icon`, className:'inline-block text-7xl text-heal-magenta'}) }
-                        <p className='block text-gen3-titanium leading-6 h-20'>{bodyText}</p>
-                        <a href={`${link}`} className='heal-btn heal-btn-rev'>{btnText}</a>
-                    </li>
-                ))}
-                </ul>
-            </div>
-            <div className='bg-heal-light_purple p-20 text-center mt-20'>
-                <div className='text-4xl'><MdFormatQuote title='quotation mark' className='inline rotate-180 text-5xl mb-2'/>Data is both a product of research, and also an engine for new discovery. <MdFormatQuote title='quotation mark' className='inline text-5xl mt-2'/></div>
-                <div className=''>REBECCA G. Baker, Ph.D., Director of the NIH HEAL Initiative</div>
-            </div>
+                }
+                return null;
+            })}
         </div>
     );
 }
--- packages/portal/src/pages/landing/index.tsx
+++ packages/portal/src/pages/landing/index.tsx
@@ -5,20 +5,19 @@ import Header, { HeaderProps } from "../../components/Navigation/Header";
 import FooterHEAL, { FooterHEALProps } from "../../components/Navigation/FooterHEAL";
 // if we want to use MDX
 // import LandingPageMDX from "../../content/landing.mdx"
-import LandingPageContent from "../../components/Contents/LandingPageContent"
-import { RoleContentEntry } from "../../components/Contents/RolesPageContent";
+import LandingPageContent, { LandingPageProps } from "../../components/Contents/LandingPageContent"
 
 interface Props extends HeaderProps {
-    rolesPages: Record<string, RoleContentEntry>
-    footer: FooterHEALProps
+    footer: FooterHEALProps,
+    landingPage: LandingPageProps
 }
 
-const LandingPage = ({ top, navigation, rolesPages, footer }: Props) => {
+const LandingPage = ({ top, navigation, footer, landingPage }: Props) => {
     return (
         <div className="flex flex-col">
             <Header top={top} navigation={navigation} />
             <div className="flex flex-row justify-items-center">
-                <LandingPageContent rolesPages={rolesPages} />
+                <LandingPageContent content={landingPage} />
             </div>
             <FooterHEAL links={footer.links} />
         </div>
@@ -32,14 +31,15 @@ export const getStaticProps: GetStaticProps = async () => {
         const json_data = JSON.parse(file_data)
         const footer_file_data = fs.readFileSync('config/footer.json', 'utf8')
         const footer_json_data = JSON.parse(footer_file_data)
-        const roles_file_data = fs.readFileSync('config/rolesPages.json', 'utf8')
-        const roles_json_data = JSON.parse(roles_file_data)
+
+        const landingPage_data = fs.readFileSync('config/landingPage.json', 'utf8');
+        const json_landingPage_data = JSON.parse(landingPage_data);
         return {
             props: {
                 navigation: json_data['navigation'],
                 top: json_data['topBar'],
-                rolesPages: roles_json_data,
-                footer: footer_json_data
+                footer: footer_json_data,
+                landingPage: json_landingPage_data
             }
         }
     } catch (err) {
@@ -49,8 +49,8 @@ export const getStaticProps: GetStaticProps = async () => {
         props: {
             navigation: {},
             top: {},
-            rolesPages: {},
-            footer: {}
+            footer: {},
+            landingPage: {}
         }
     }
 }
