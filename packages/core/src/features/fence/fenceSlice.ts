0fed831afa11b11ee47afc0abe2c1ec800d5817c
Craig Barnes <craigrbarnes@uchicago.edu>
2022-04-23 17:54:33 -0500

    add fence login providers

---
+++ packages/core/src/features/fence/fenceSlice.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { CoreDispatch } from "../../store";
+import { CoreState } from "../../reducers";
+import { fetchLogin,Gen3FenceResponce, Gen3FenceUserProviders  } from "./fenceApi";
+import { DataStatus, CoreDataSelectorResponse } from "../../dataAccess";
+
+export const fetchLoginProviders = createAsyncThunk<
+    Gen3FenceResponce<Gen3FenceUserProviders>,
+    string,
+    { dispatch: CoreDispatch; state: CoreState }
+    >("fence/user/login", async (hostname: string) => {
+        return await fetchLogin(hostname);
+    }
+    );
+
+export interface Gen3LoginProviderState extends Gen3FenceUserProviders {
+    readonly status: DataStatus;
+    readonly error?: string;
+}
+
+
+const initialState: Gen3LoginProviderState = {
+    default_provider: { idp:"", desc: "", id: "", name:"", url:"", secondary: false, urls: []},
+    providers: undefined,
+    status: "uninitialized",
+    error: undefined
+};
+
+const slice = createSlice({
+    name: "facets",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchLoginProviders.fulfilled, (state, action) => {
+                const response = action.payload;
+                state.default_provider = response.data.default_provider;
+                state.status = "fulfilled";
+            })
+            .addCase(fetchLoginProviders.pending, (state ) => {
+                state.status =  "pending";
+            })
+            .addCase(fetchLoginProviders.rejected, (state) => {
+                state.status =  "rejected";
+                }
+            );
+    },
+});
+
+export const loginProvidersReducer = slice.reducer;
+
+export const selectLoginProvidersState = (state: CoreState): Gen3LoginProviderState => state.fence;
+
+export const selectLoginProvidersData = (
+    state: CoreState,
+): CoreDataSelectorResponse<Gen3FenceUserProviders> => {
+    return {
+        data: {
+            default_provider: state.fence.default_provider,
+            providers: state.fence.providers
+        },
+        status: state.fence.status,
+        error: state.fence.error,
+    };
+};
