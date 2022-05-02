beb7b249177c6c0c6c80383031fbae1f5b7ef048
Mingfei Shao <mshao1@uchicago.edu>
2022-05-02 16:04:00 -0500

    remove unused pages

--- packages/portal/src/pages/landing/index.tsx
+++ packages/portal/src/pages/landing/index.tsx
@@ -3,7 +3,6 @@ import { GetStaticProps } from "next";
 // if we want to use MDX
 // import LandingPageMDX from "../../content/landing.mdx"
 import LandingPageContent, { LandingPageProps } from "../../components/Contents/LandingPageContent"
-import { RoleContentEntry } from "../../components/Contents/RolesPageContent";
 import { getNavPageLayoutPropsFromConfig } from "../../common/staticProps";
 import ContentSource from "../../lib/content";
 import NavPageLayout, { NavPageLayoutProps } from "../../components/Navigation/NavPageLayout";
@@ -25,12 +24,10 @@ const LandingPage = ({ headerProps, footerProps, landingPage }: Props) => {
 // should move this thing into _app.tsx and make a dedicated layout component after https://github.com/vercel/next.js/discussions/10949 is addressed
 export const getStaticProps: GetStaticProps = async () => {
     const navPageLayoutProps = await getNavPageLayoutPropsFromConfig();
-    const rolesPages = await ContentSource.get('config/rolesPages.json') as unknown as Record<string, RoleContentEntry>;
     const landingPage = await ContentSource.get('config/landingPage.json');
     return {
         props: {
             ...navPageLayoutProps,
-            rolesPages,
             landingPage
         }
     }
