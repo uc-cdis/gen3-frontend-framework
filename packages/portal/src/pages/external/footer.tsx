b1442e7e7fe4ad6e86c25f4281b6785319a0351c
Matthew Cannalte <mcannalte@uchicago.edu>
2022-05-25 09:54:57 -0500

    feat(external-footer):

--- packages/portal/src/pages/external/footer.tsx
+++ packages/portal/src/pages/external/footer.tsx
@@ -5,7 +5,6 @@ import ContentSource from '../../lib/content';
 import Footer, { FooterProps } from '../../components/Navigation/Footer';
 
 const StandaloneFooterPage = (props: FooterProps) => {
-    console.log(props)
   return (
     <Footer {...props}/>
   );
@@ -14,7 +13,6 @@ const StandaloneFooterPage = (props: FooterProps) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const {footerProps} = await getNavPageLayoutPropsFromConfig();
-  const landingPage = await ContentSource.get('config/landingPage.json');
   return {
     props: {
       ...footerProps
