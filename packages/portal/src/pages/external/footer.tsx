12b8e8d1ecfb773de1ee485917f1127b2dc7ef0e
Matthew Cannalte <mcannalte@uchicago.edu>
2022-06-02 15:46:40 -0500

    feat(external-footer): set base target

--- packages/portal/src/pages/external/footer.tsx
+++ packages/portal/src/pages/external/footer.tsx
@@ -1,12 +1,17 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getNavPageLayoutPropsFromConfig } from '../../common/staticProps';
 import Footer, { FooterProps } from '../../components/Navigation/Footer';
 
 const StandaloneFooterPage = (props: FooterProps) => {
-  return (
+  return <>
+    <Head>
+      {/* footer links should open in parent (probably data portal) frame */}
+      <base target='_parent'/>
+    </Head>
     <Footer {...props}/>
-  );
+  </>;
 };
 
 
