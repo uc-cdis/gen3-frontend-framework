d8d907622dc8723f4bc099a0c363cbbcdc3a121f
Craig Barnes <craigrbarnes@uchicago.edu>
2022-07-22 15:11:35 -0500

    update fence config

--- packages/core/src/features/fence/fenceSlice.ts
+++ packages/core/src/features/fence/fenceSlice.ts
@@ -12,9 +12,8 @@ export const fetchLoginProviders = createAsyncThunk<
     string,
     { dispatch: CoreDispatch; state: CoreState }
     >('fence/user/login', async (hostname: string, thunkAPI) => {
-        const csrfToken = selectCSRFToken(thunkAPI.getState());
-        console.log(csrfToken);
-        return await fetchLogin(hostname, csrfToken);
+      const csrfToken = selectCSRFToken(thunkAPI.getState());
+      return await fetchLogin(hostname, csrfToken);
     }
     );
 
@@ -25,37 +24,37 @@ export interface Gen3LoginProviderState extends Gen3FenceUserProviders {
 
 
 const initialState: Gen3LoginProviderState = {
-    default_provider: { idp:'', desc: '', id: '', name:'', url:'', secondary: false, urls: []},
-    providers: [],
-    status: 'uninitialized',
-    error: undefined
+  default_provider: { idp:'', desc: '', id: '', name:'', url:'', secondary: false, urls: []},
+  providers: [],
+  status: 'uninitialized',
+  error: undefined
 };
 
 const slice = createSlice({
-    name: 'facets',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchLoginProviders.fulfilled, (state, action) => {
-                const response = action.payload;
-                if (response.errors) {
-                    state = castDraft(initialState);
-                    state.status = 'rejected';
-                    state.error = response.errors.filters;
-                }
+  name: 'facets',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchLoginProviders.fulfilled, (state, action) => {
+        const response = action.payload;
+        if (response.errors) {
+          state = castDraft(initialState);
+          state.status = 'rejected';
+          state.error = response.errors.filters;
+        }
 
-                state = { ...response.data, status: 'fulfilled' };
-                return state;
-            })
-            .addCase(fetchLoginProviders.pending, (state ) => {
-                state.status =  'pending';
-            })
-            .addCase(fetchLoginProviders.rejected, (state) => {
-                state.status =  'rejected';
-                }
-            );
-    },
+        state = { ...response.data, status: 'fulfilled' };
+        return state;
+      })
+      .addCase(fetchLoginProviders.pending, (state ) => {
+        state.status =  'pending';
+      })
+      .addCase(fetchLoginProviders.rejected, (state) => {
+        state.status =  'rejected';
+      }
+      );
+  },
 });
 
 export const loginProvidersReducer = slice.reducer;
@@ -63,14 +62,14 @@ export const loginProvidersReducer = slice.reducer;
 export const selectLoginProvidersState = (state: CoreState): Gen3LoginProviderState => state.fence;
 
 export const selectLoginProvidersData = (
-    state: CoreState,
+  state: CoreState,
 ): CoreDataSelectorResponse<Gen3FenceUserProviders> => {
-    return {
-        data: {
-            default_provider: state.fence.default_provider,
-            providers: state.fence.providers
-        },
-        status: state.fence.status,
-        error: state.fence.error,
-    };
+  return {
+    data: {
+      default_provider: state.fence.default_provider,
+      providers: state.fence.providers
+    },
+    status: state.fence.status,
+    error: state.fence.error,
+  };
 };
