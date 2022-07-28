72951c9e1c5b7606e63cd99312cd4c847fd46a6a
Craig Barnes <craigrbarnes@uchicago.edu>
2022-07-28 09:46:55 -0500

    fix lint errors

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -2,7 +2,7 @@ import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
-import json from "@rollup/plugin-json";
+import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const globals = {
