224f3d02676731a74e468b95c4991535d88f617a
Craig Barnes <craigrbarnes@uchicago.edu>
2022-09-19 07:12:52 -0500

    fix lint errors

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -1,16 +1,18 @@
-import {buildCreateApi, coreModule, reactHooksModule} from '@reduxjs/toolkit/dist/query/react';
-import {useCoreSelector , useCoreStore, useCoreDispatch} from './hooks';
+import { buildCreateApi, coreModule, reactHooksModule } from '@reduxjs/toolkit/dist/query/react';
+import { useCoreSelector, useCoreStore, useCoreDispatch } from './hooks';
 
 
 export const coreCreateApi = buildCreateApi(
   coreModule(),
   reactHooksModule({
-      // TODO properly type the next two declarations.
+
+    // TODO properly type the next two declarations.
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    useSelector:  useCoreSelector,
+    useSelector: useCoreSelector,
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     useStore: useCoreStore,
-    useDispatch:  useCoreDispatch })
+    useDispatch: useCoreDispatch,
+  }),
 );
