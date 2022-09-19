9d9b8c521a2aa4698bd6db209c9b1c9112253014
Craig Barnes <craigrbarnes@uchicago.edu>
2022-09-19 06:55:17 -0500

    add TODO in api

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -5,6 +5,7 @@ import {useCoreSelector , useCoreStore, useCoreDispatch} from './hooks';
 export const coreCreateApi = buildCreateApi(
   coreModule(),
   reactHooksModule({
+      // TODO properly type the next two declarations.
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     useSelector:  useCoreSelector,
