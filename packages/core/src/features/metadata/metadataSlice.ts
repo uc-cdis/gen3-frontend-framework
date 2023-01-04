fa10e2707f189cb107b03422d1e9b48f6efdd87c
Craig Barnes <craigrbarnes@uchicago.edu>
2023-01-04 13:31:12 -0600

    pretty metadataSlice.ts

--- packages/core/src/features/metadata/metadataSlice.ts
+++ packages/core/src/features/metadata/metadataSlice.ts
@@ -1,9 +1,9 @@
-import {  fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { coreCreateApi } from '../../api';
-import {Reducer, Middleware} from '@reduxjs/toolkit';
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { coreCreateApi } from "../../api";
+import { Reducer, Middleware } from "@reduxjs/toolkit";
 
 export interface Metadata {
-    readonly entries:Array<Record<string, unknown>>
+  readonly entries: Array<Record<string, unknown>>;
 }
 
 export interface CrosswalkInfo {
@@ -12,7 +12,7 @@ export interface CrosswalkInfo {
 }
 
 export interface CrosswalkArray {
-  readonly mapping : ReadonlyArray<CrosswalkInfo>
+  readonly mapping: ReadonlyArray<CrosswalkInfo>;
 }
 
 interface CrossWalkParams {
@@ -20,37 +20,47 @@ interface CrossWalkParams {
   readonly fields: {
     from: string;
     to: string;
-  }
+  };
 }
 
 // Define a service using a base URL and expected endpoints
 export const metadataApi = coreCreateApi({
-  reducerPath: 'metadataApi',
+  reducerPath: "metadataApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://brh.data-commons.org/mds/' }),
+    baseUrl: "https://brh.data-commons.org/mds/",
+  }),
   endpoints: (builder) => ({
     getMetadata: builder.query<Metadata, string>({
-      query: () => 'aggregate/metadata',
+      query: () => "aggregate/metadata",
     }),
     getTags: builder.query<Metadata, string>({
-      query: () => 'tags',
+      query: () => "tags",
     }),
     getData: builder.query<Metadata, string>({
-      query: (params) => ( { url: `metadata?${params}`} )
+      query: (params) => ({ url: `metadata?${params}` }),
     }),
     getCrosswalkData: builder.query<CrosswalkArray, CrossWalkParams>({
-      query: (params) => ( { url: `metadata?${params.ids}`} ),
-      transformResponse: (response: Record<string, any>, _meta, params ) => {
-        return { mapping :  Object.values(response).map((x): CrosswalkInfo => {
-          return { from: x.ids[params.fields.from], to: x.ids[params.fields.to] };
-        })};
-      }
+      query: (params) => ({ url: `metadata?${params.ids}` }),
+      transformResponse: (response: Record<string, any>, _meta, params) => {
+        return {
+          mapping: Object.values(response).map((x): CrosswalkInfo => {
+            return {
+              from: x.ids[params.fields.from],
+              to: x.ids[params.fields.to],
+            };
+          }),
+        };
+      },
     }),
   }),
 });
 
-
-export const { useGetMetadataQuery, useGetTagsQuery, useGetDataQuery, useGetCrosswalkDataQuery } = metadataApi;
-export const mdsReducerPath:string = metadataApi.reducerPath;
+export const {
+  useGetMetadataQuery,
+  useGetTagsQuery,
+  useGetDataQuery,
+  useGetCrosswalkDataQuery,
+} = metadataApi;
+export const mdsReducerPath: string = metadataApi.reducerPath;
 export const mdsReducer: Reducer = metadataApi.reducer as Reducer;
 export const mdsReducerMiddleware = metadataApi.middleware as Middleware;
