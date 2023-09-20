6bb1e46584e48146cfdca8ee80af09065809aa14
craigrbarnes <craigrbarnes@uchicago.edu>
2023-09-19 23:10:01 -0500

    fix lint lint error in frontend/jest.config.ts

--- packages/frontend/jest.config.ts
+++ packages/frontend/jest.config.ts
@@ -1,10 +1,7 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
-  // [...]
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
     '^.+\\.[tj]sx?$': [
       'ts-jest',
       {
