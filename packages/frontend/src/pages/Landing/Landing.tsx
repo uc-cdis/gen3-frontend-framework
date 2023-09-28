73106ae43385830cd9a834ca6051c2a601cdc727
craigrbarnes <craigrbarnes@uchicago.edu>
2023-09-28 14:20:40 -0500

    Add Landing page to index

---
+++ packages/frontend/src/pages/Landing/Landing.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// if we want to use MDX
+// import LandingPageMDX from "../../content/landing.mdx"
+import LandingPageContent, { LandingPageProps } from "../../components/Content/LandingPageContent";
+import NavPageLayout, { NavPageLayoutProps } from "../../features/Navigation/NavPageLayout";
+
+interface Props extends NavPageLayoutProps {
+  landingPage: LandingPageProps;
+}
+
+const LandingPage = ({ headerProps, footerProps, landingPage }: Props) => {
+  return (
+    <NavPageLayout {...{ footerProps, headerProps }}>
+      <div className="flex flex-row justify-items-center">
+        <LandingPageContent content={landingPage} />
+      </div>
+    </NavPageLayout>
+  );
+};
+
+export default LandingPage;
