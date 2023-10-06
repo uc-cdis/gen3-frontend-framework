aaf64e7a98208fe5c9c79948108916e16bee32bd
craigrbarnes <craigrbarnes@uchicago.edu>
2023-10-06 11:39:57 -0500

    fix compile issues

--- packages/frontend/src/components/table/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ExplorerTable';
-export  { TableCellRenderer }  from './CellRenderers';
-export { jsonPathAccessor } from './utils';
-export { type ColumnDefinition,   } from './types';
---
+++ packages/frontend/src/features/CohortBullder/ExplorerTable/index.ts
@@ -0,0 +1,11 @@
+import ExplorerTable from './ExplorerTable';
+import { TableCellRenderer }  from './CellRenderers';
+import { jsonPathAccessor } from './utils';
+import { type ColumnDefinition,  } from './types';
+
+export {
+  ExplorerTable,
+  TableCellRenderer,
+  jsonPathAccessor,
+  type ColumnDefinition,
+};
