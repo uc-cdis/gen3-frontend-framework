4a335a4c18041722d3ef24d4d2166e51256534ce
craigrbarnes <craigrbarnes@uchicago.edu>
2024-02-21 18:12:13 -0600

    test BRH

--- packages/frontend/rollup.config.mjs
+++ packages/frontend/rollup.config.mjs
@@ -60,9 +60,17 @@ const config = [
         dir: 'dist',
         format: 'esm',
         name: 'gen3frontend',
+        plugins: [terser()],
         globals,
         sourcemap: true
-      }
+      },
+      {
+        dir: 'dist',
+        format: 'esm',
+        name: 'gen3Core',
+        globals,
+        sourcemap: true
+      },
     ],
     external: [
       ...Object.keys(globals),
