e7a654f5c9ef459e4d9388120af4c36edd72b3b1
craigrbarnes <craigrbarnes@uchicago.edu>
2024-03-26 17:14:55 -0500

    add Custom404Page, handle missing config

--- packages/frontend/src/pages/Discovery/data.ts
+++ packages/frontend/src/pages/Discovery/data.ts
@@ -3,19 +3,29 @@ import { GEN3_COMMONS_NAME } from '@gen3/core';
 import { getNavPageLayoutPropsFromConfig } from '../../lib/common/staticProps';
 import ContentSource from '../../lib/content';
 import { type DiscoveryProps } from '../../features/Discovery/Discovery';
-import { type NavPageLayoutProps } from '../../features/Navigation';
+import type { NavPageLayoutProps } from '../../features/Navigation';
 
 export const DiscoveryPageGetServerSideProps: GetServerSideProps<
   NavPageLayoutProps
 > = async () => {
-  const discoveryConfig: DiscoveryProps = await ContentSource.get(
-    `config/${GEN3_COMMONS_NAME}/discovery.json`,
-  );
 
-  return {
-    props: {
-      ...(await getNavPageLayoutPropsFromConfig()),
-       discoveryConfig: discoveryConfig,
-    },
-  };
+  try {
+    const discoveryConfig: DiscoveryProps = await ContentSource.get(
+      `config/${GEN3_COMMONS_NAME}/discovery.json`,
+    );
+    return {
+      props: {
+        ...(await getNavPageLayoutPropsFromConfig()),
+        discoveryConfig: discoveryConfig,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: {
+        ...(await getNavPageLayoutPropsFromConfig()),
+        discoveryConfig: undefined,
+      },
+    };
+  }
 };
