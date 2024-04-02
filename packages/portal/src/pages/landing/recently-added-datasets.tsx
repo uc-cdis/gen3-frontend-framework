773a0ec4677caa45c36ef21afefd15f2db4db77f
Piotr Senkow <piotrsenkow@gmail.com>
2024-04-02 10:22:49 -0500

    refactored ResourcePageContent to CardedPageContent since both the resource and recently added datasets page use the same HTML structure

--- packages/portal/src/pages/landing/recently-added-datasets.tsx
+++ packages/portal/src/pages/landing/recently-added-datasets.tsx
@@ -2,20 +2,20 @@ import {GetStaticProps} from 'next';
 import ContentSource from '../../lib/content';
 
 import NavPageLayout, { NavPageLayoutProps } from '../../components/Navigation/NavPageLayout';
-import ResourcePageContent, {ResourcePageConfig} from '../../components/Contents/ResourcePageContent';
+import CardedPageContent, {CardedPageConfig} from '../../components/Contents/CardedPageContent';
 import { getNavPageLayoutPropsFromConfig } from '../../common/staticProps';
 
 interface ResourcePageProps extends NavPageLayoutProps {
-    resourcePageConfig: ResourcePageConfig
+    newDatasetsPageConfig: CardedPageConfig
 }
 
-const ResourcePage = ({headerProps, footerProps, resourcePageConfig}: ResourcePageProps) => {
+const ResourcePage = ({headerProps, footerProps, newDatasetsPageConfig}: ResourcePageProps) => {
 
   return (
     <NavPageLayout {...{headerProps, footerProps}}>
       <div className='flex flex-row  justify-items-center'>
         <div className='sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-xl mx-20'>
-          <ResourcePageContent {...resourcePageConfig}/>
+          <CardedPageContent {...newDatasetsPageConfig}/>
         </div>
       </div>
     </NavPageLayout>
@@ -26,11 +26,11 @@ const ResourcePage = ({headerProps, footerProps, resourcePageConfig}: ResourcePa
 // should move this thing into _app.tsx and make a dedicated layout component after https://github.com/vercel/next.js/discussions/10949 is addressed
 export const getStaticProps: GetStaticProps<ResourcePageProps> = async ( ) => {
   const navPageLayoutProps = await getNavPageLayoutPropsFromConfig();
-  const resourcePageConfig = await ContentSource.get('config/newDatasets.json') as unknown as ResourcePageConfig;
+  const newDatasetsPageConfig = await ContentSource.get('config/newDatasets.json') as unknown as CardedPageConfig;
   return {
     props: {
       ...navPageLayoutProps,
-      resourcePageConfig
+      newDatasetsPageConfig
     }
   };
 };
