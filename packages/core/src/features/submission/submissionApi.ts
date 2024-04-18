afdbc5a33177da3465985805b38000de37319934
craigrbarnes <craigrbarnes@uchicago.edu>
2024-04-18 09:36:35 -0500

    add submissionApi

---
+++ packages/core/src/features/submission/submissionApi.ts
@@ -0,0 +1,9 @@
+import { gen3Api } from '../gen3';
+import { GEN3_API } from '../../constants';
+
+
+export const submissionApi = gen3Api.injectEndpoints({
+  endpoints: (builder) => ({
+
+  }),
+});
