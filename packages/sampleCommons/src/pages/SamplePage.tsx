b1fe4ef068a1f220ba97141f6659b5d9ffce3b4a
craigrbarnes <craig.barnes@intelligentlight.com>
2024-05-01 12:05:44 -0500

    Fixes content of color page, layout typing  (#174)
    * fix styling on LandingPage/ColorTheme
    
    * fix colors
    
    * add email contact support for Login Panel
    
    * adjust tooltip style
    
    * add tooltip to LoginButton
    
    * add authz to Discovery data loaders
    
    * adjust data Authz
    
    * Add downloadLinks to DetailsPanel
    
    * fix compile errors
    
    * Move documentation
    Fix vertical navigation tailwind overrides
    
    * update types
    
    * sample popup
    
    * fix types
    
    * v0.10.24
    
    ---------
    
    Co-authored-by: craigrbarnes <craigrbarnes@uchicago.edu>

--- packages/sampleCommons/src/pages/SamplePage.tsx
+++ packages/sampleCommons/src/pages/SamplePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Paper, Grid, Title, Group } from '@mantine/core';
+import { Center, Text, Paper } from '@mantine/core';
 import {
   NavPageLayout,
   NavPageLayoutProps,
@@ -36,86 +36,24 @@ interface DataComponentProps {
 }
 const DataComponent = ({ data }: DataComponentProps) => {
   return (
-    <Grid>
-      {data.map((item) => (
-        <Grid.Col key={item.id} span={4} style={{ marginBottom: 5, marginTop: 5 }}>
-          <Paper style={{ padding: 'lg', boxShadow: 'xs' }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Text style={{ margin: 10 }}>  ✅  </Text>
-              <div>
-                <div>Subject: {item.subject}</div>
-              </div>
-            </div>
-          </Paper>
-        </Grid.Col>
-      ))}
-    </Grid>
-  );
-};
-
-const query =
-  `query($filter: JSON){
-    file(filter: $filter first: 10000){
-      subject
-    }
-   }`;
-
-const variables = {
-  filter: {
-    AND: [{ IN: { project_id: ['synthea-test'] } }],
-  },
-};
-
-const SamplePage = ({ headerProps, footerProps }: SamplePageProps) => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [isLoading, setLoading] = useState<boolean>(true);
-
-  const  headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  };
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result: GuppyAPIFetchResult = await guppyAPIFetch({
-          query: query,
-          variables: variables,
-        }, headers);
-        setItems(result.data.file);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
     <NavPageLayout {...{ headerProps, footerProps }}>
       <div className="w-full m-10">
         <Center>
         <Paper shadow="md" p="xl" withBorder>
+          <Text>This is a example custom page in Gen3</Text>
           <Text>
-            Demo report page under construction
+            You can add your own content here, and add a link to this page in
+            the navigation bar by editing the config file in
+            navigation.json
           </Text>
         </Paper>
         </Center>
       </div>
-      {isLoading ? (
-        <p>Loading data...</p>
-      ) : (
-        <Group style={{ margin: 20 }}>
-        <Title> Synthetic Data FHIR references </Title>
-        <DataComponent data={items} />
-        </Group>
-      )}
     </NavPageLayout>
   );
 };
 
-
+// TODO: replace this with a custom getServerSideProps function
 export const getServerSideProps: GetServerSideProps<
   NavPageLayoutProps
 > = async () => {
