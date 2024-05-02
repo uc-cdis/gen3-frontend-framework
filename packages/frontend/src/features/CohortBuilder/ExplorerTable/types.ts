769782b5034574bd64b4dce82b8334e88700354d
craigrbarnes <craigrbarnes@uchicago.edu>
2024-05-02 17:22:54 -0500

    add pageLimit support for Explorer table

--- packages/frontend/src/features/CohortBuilder/ExplorerTable/types.ts
+++ packages/frontend/src/features/CohortBuilder/ExplorerTable/types.ts
@@ -4,20 +4,35 @@ export interface ColumnDefinition {
   className?: string; // for use with tailwind
 }
 
-export type SummaryTableColumnType = 'string' | 'number' | 'date' | 'array' | 'link' | 'boolean' | 'paragraphs';
+export type SummaryTableColumnType =
+  | 'string'
+  | 'number'
+  | 'date'
+  | 'array'
+  | 'link'
+  | 'boolean'
+  | 'paragraphs';
 
 export interface SummaryTableColumn {
-  field: string;
-  title: string;
+  field: string; // field name
+  title: string; // column title
   accessorPath?: string; // JSONPath to column data
-  type?: SummaryTableColumnType,
-  cellRenderFunction?: string,
-  params?: Record<string, any>;
+  type?: SummaryTableColumnType; // type of column data
+  cellRenderFunction?: string; // name of cell renderer function
+  params?: Record<string, any>; // additional parameters for cell renderer
   width?: number; // override auto width of column
+  sortable?: boolean; // enable sorting on this column
+  visible?: boolean; // show/hide column
+}
+
+export interface SummaryTablePageLimit {
+  readonly limit?: number; // default number of total items to page trough
+  readonly label?: string; // message to display next to pagination controls
 }
 
 export interface SummaryTable {
   readonly enabled: boolean;
   readonly fields: ReadonlyArray<string>;
   readonly columns?: Record<string, SummaryTableColumn>;
+  readonly pageLimit?: SummaryTablePageLimit;
 }
--- packages/frontend/src/features/CohortBuilder/types.ts
+++ packages/frontend/src/features/CohortBuilder/types.ts
@@ -1,4 +1,3 @@
-
 // set of interfaces which follows the current explorer configuration
 
 import { SummaryChart } from '../../components/charts';
@@ -9,69 +8,69 @@ import { Dispatch, SetStateAction } from 'react';
 import { Modals } from '@gen3/core';
 
 export interface TabConfig {
-    readonly title: string;
-    readonly fields: ReadonlyArray<string>;
+  readonly title: string;
+  readonly fields: ReadonlyArray<string>;
 }
 
 export interface TabsConfig {
-    readonly tabs: ReadonlyArray<TabConfig>;
+  readonly tabs: ReadonlyArray<TabConfig>;
 }
 
 export interface ManifestFieldsConfig {
-    resourceIndexType: string;
-    resourceIdField: string;
-    referenceIdFieldInResourceIndex: string;
-    referenceIdFieldInDataIndex: string;
+  resourceIndexType: string;
+  resourceIdField: string;
+  referenceIdFieldInResourceIndex: string;
+  referenceIdFieldInDataIndex: string;
 }
 
 export interface DataTypeConfig {
-    readonly dataType: string;
-    readonly nodeCountTitle: string;
-    readonly fieldMapping: ReadonlyArray<FieldToName>;
-    readonly manifestMapping?: ManifestFieldsConfig;
-    readonly accessibleFieldCheckList?: string[];
-    readonly accessibleValidationField?: string;
+  readonly dataType: string;
+  readonly nodeCountTitle: string;
+  readonly fieldMapping: ReadonlyArray<FieldToName>;
+  readonly manifestMapping?: ManifestFieldsConfig;
+  readonly accessibleFieldCheckList?: string[];
+  readonly accessibleValidationField?: string;
 }
 
 export interface DownloadButtonConfig extends DownloadButtonProps {
-    dropdownId?: string;
+  dropdownId?: string;
 }
 
 export interface DropdownButtonsConfig {
-    readonly title: string;
+  readonly title: string;
 }
 
 export interface DropdownsWithButtonsProps extends DropdownButtonsConfig {
-    dropdownItems: ReadonlyArray<DownloadButtonProps>;
+  dropdownItems: ReadonlyArray<DownloadButtonProps>;
 }
 
 export interface CohortPanelConfig {
-    readonly guppyConfig: DataTypeConfig;
-    readonly tabTitle: string;
-    readonly charts?: Record<string, SummaryChart>;
-    readonly table?: SummaryTable;
-    readonly filters?: TabsConfig;
-    readonly dropdowns?: Record<string, DropdownsWithButtonsProps>;
-    readonly buttons?: ReadonlyArray<DownloadButtonConfig>;
-    readonly loginForDownload?: boolean;
+  readonly guppyConfig: DataTypeConfig; // guppy config
+  readonly tabTitle: string; // title of the tab
+  readonly charts?: Record<string, SummaryChart>; // grid of charts
+  readonly table?: SummaryTable; // table configuration
+  readonly filters?: TabsConfig; // filters for the fields
+  readonly dropdowns?: Record<string, DropdownsWithButtonsProps>; // dropdown menu of action buttons
+  readonly buttons?: ReadonlyArray<DownloadButtonConfig>; // row of action buttons
+  readonly loginForDownload?: boolean; // login required for download
 }
 
 export interface CohortBuilderConfiguration {
-    explorerConfig: Array<CohortPanelConfig>;
+  explorerConfig: Array<CohortPanelConfig>;
 }
 
 // to do add buttons, options,  menus, etc
 
 export interface CohortConfig {
-    tabs: TabConfig[];
+  tabs: TabConfig[];
 }
 
 export enum DownloadFileFormats {
-    JSON = 'JSON',
-    CSV = 'CSV',
-    TSV = 'TSV',
-    DATA = 'DATA',
-    UNDEFINED = 'UNDEFINED',
+  JSON = 'JSON',
+  CSV = 'CSV',
+  TSV = 'TSV',
+  DATA = 'DATA',
+  UNDEFINED = 'UNDEFINED',
 }
 
 export type ActionButtonFunction = (
@@ -89,30 +88,29 @@ export type ActionButtonWithArgsFunction = (
   signal?: AbortSignal,
 ) => Promise<void>;
 
-
-export interface DownloadButtonPropsWithAction extends Omit<DownloadButtonProps, 'action' |'actionArgs'> {
-    actionFunction: ActionButtonWithArgsFunction;
-    actionArgs: Record<string, any>;
-
+export interface DownloadButtonPropsWithAction
+  extends Omit<DownloadButtonProps, 'action' | 'actionArgs'> {
+  actionFunction: ActionButtonWithArgsFunction;
+  actionArgs: Record<string, any>;
 }
 
 export interface GuppyActionButtonProps {
-    disabled?: boolean;
-    inactiveText: string;
-    activeText: string;
-    customStyle?: string;
-    showLoading?: boolean;
-    showIcon?: boolean;
-    preventClickEvent?: boolean;
-    onClick?: () => void;
-    setActive?: Dispatch<SetStateAction<boolean>>;
-    active?: boolean;
-    Modal403?: Modals;
-    Modal400?: Modals;
-    tooltipText?: string;
-    done?: () => void;
-    actionFunction: ActionButtonWithArgsFunction;
-    actionArgs: Record<string, any>;
-    customErrorMessage?: string;
-    hideNotification?: boolean;
+  disabled?: boolean;
+  inactiveText: string;
+  activeText: string;
+  customStyle?: string;
+  showLoading?: boolean;
+  showIcon?: boolean;
+  preventClickEvent?: boolean;
+  onClick?: () => void;
+  setActive?: Dispatch<SetStateAction<boolean>>;
+  active?: boolean;
+  Modal403?: Modals;
+  Modal400?: Modals;
+  tooltipText?: string;
+  done?: () => void;
+  actionFunction: ActionButtonWithArgsFunction;
+  actionArgs: Record<string, any>;
+  customErrorMessage?: string;
+  hideNotification?: boolean;
 }
