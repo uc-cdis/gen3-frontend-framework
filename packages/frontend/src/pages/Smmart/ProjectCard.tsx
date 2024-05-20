29606afc9e900838ae675b46ba45fb0bd333ce51
quinnwai <quinnwai.wong@gmail.com>
2024-05-20 15:37:17 -0700

    standardize CBDS homepage cards

---
+++ packages/frontend/src/pages/Smmart/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import { Text, Button, Card, Image } from '@mantine/core';
+
+interface HomepageCardProps {
+    icon: string;
+    title: string;
+    description: string;
+  }
+
+const ProjectCard = ({title, description, icon}: HomepageCardProps) =>
+(
+    <Card className="shadow-lg p-6 text-left flex-1 basis-[20%] m-[13%]">
+    <div className="flex items-left space-x-4 p-3">
+        <div className="w-1/5">
+        <Image className="w-1/2" src={icon} alt={`${title} logo`}/>
+        </div>
+        <Text className= "text-2xl text-left font-bold">{title}</Text>
+    </div>
+    <Text className="p-5 text-left">{description}</Text>
+    <div className="pb-5">
+    <Button className="py-3 w-full h-1/6 !bg-cbds-secondary hover:!bg-cbds-monosecondary text-white text-lg">Explore</Button>
+    </div>
+    </Card>
+);
+
+export default ProjectCard;
