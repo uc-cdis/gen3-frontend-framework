a34b26482dbe750597eb2ece1df48aebd7302340
Craig Barnes <craig.barnes@intelligentlight.com>
2024-08-26 18:19:55 -0500

    fix session effect (#232)

--- packages/frontend/src/lib/session/session.tsx
+++ packages/frontend/src/lib/session/session.tsx
@@ -246,7 +246,7 @@ export const SessionProvider = ({
       window.removeEventListener('mousedown', updateUserActivity);
       window.removeEventListener('keypress', updateUserActivity);
     };
-  }, [updateSession, updateSessionIntervalMilliseconds]);
+  }, []); // only call on mount/dismount
 
   useInterval(
     () => {
