78acf1a701c6734592819c4326ac70217efd0d9d
craigrbarnes <craig.barnes@intelligentlight.com>
2024-08-28 17:03:58 -0500

    merge from develop

--- packages/frontend/src/pages/Workspace/Workspace.tsx
+++ packages/frontend/src/pages/Workspace/Workspace.tsx
@@ -9,7 +9,14 @@ const WorkspacePage = ({
   workspaceProps,
 }: WorkspacePageLayoutProps): JSX.Element => {
   return (
-    <NavPageLayout {...{ headerProps, footerProps }}>
+    <NavPageLayout
+      {...{ headerProps, footerProps }}
+      headerData={{
+        title: 'Gen3 Workspace Page',
+        content: 'Workspace page',
+        key: 'gen3-workspace-page',
+      }}
+    >
       <Workspace config={workspaceProps} />
     </NavPageLayout>
   );
