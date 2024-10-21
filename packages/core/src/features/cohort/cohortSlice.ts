6b3fb2aed87b7f7484a6ad07336108d606cc55f6
craigrbarnes <craig.barnes@intelligentlight.com>
2024-10-21 12:28:52 -0500

    update cohort representation

--- packages/core/src/features/cohort/cohortSlice.ts
+++ packages/core/src/features/cohort/cohortSlice.ts
@@ -1,27 +1,50 @@
-import { createSlice, Draft, type PayloadAction } from '@reduxjs/toolkit';
+import {
+  createEntityAdapter,
+  createSlice,
+  EntityState,
+  type PayloadAction,
+  nanoid,
+} from '@reduxjs/toolkit';
 import type { CoreState } from '../../reducers';
 import { Operation, FilterSet, IndexedFilterSet } from '../filters';
+import { defaultCohortNameGenerator } from './utils';
 
+/**
+ *  Cohorts in Gen3 are defined as a set of filters for each index in the data.
+ *  This means one cohort id defined for all "tabs" in CohortBuilder (explorer)
+ *  Switching a cohort is means that all the cohorts for the index changes.
+ */
+
+export const UNSAVED_COHORT_NAME = 'Unsaved_Cohort';
+
+type CohortId = string;
+
+/**
+ * A Cohort is a collection of filters that can be used to query the GDC API.
+ * The cohort interface is used to manage the cohort state in the redux-toolkit entity adapter.
+ * @see https://redux-toolkit.js.org/api/createEntityAdapter
+ *
+ * @property id - the id of the cohort
+ * @property name - the name of the cohort
+ * @property filters - the filters for the cohort
+ * @property modified - flag indicating if the cohort has been modified
+ * @property modified_datetime - the last time the cohort was modified
+ * @property saved - flag indicating if the cohort has been saved
+ * @category Cohort
+ */
 export interface Cohort {
-  readonly id: string;
-  readonly name: string;
-  readonly filters: IndexedFilterSet; // maps of index to filter set
-  readonly modified?: boolean; // flag which is set to true if modified and unsaved
-  readonly modified_datetime: string; // last time cohort was modified
+  id: CohortId;
+  name: string;
+  filters: IndexedFilterSet; // maps of index to filter set
+  modified?: boolean; // flag which is set to true if modified and unsaved
+  modified_datetime: string; // last time cohort was modified
+  saved?: boolean; // flag indicating if cohort has been saved.
 }
 
-export interface CohortState {
-  cohort: Cohort;
+export interface CurrentCohortState {
+  currentCohort?: string;
+  message?: string[];
 }
-
-const initialCohortState: CohortState = {
-  cohort: {
-    id: 'default',
-    name: 'Filters',
-    filters: {},
-    modified_datetime: new Date().toISOString(),
-  },
-};
 
 interface UpdateFilterParams {
   index: string;
@@ -34,19 +57,61 @@ interface RemoveFilterParams {
   field: string;
 }
 
+export const createCohortName = (postfix: string): string => {
+  return `Custom Cohort ${postfix}`;
+};
+
 interface ClearAllFilterParams {
   index: string;
 }
 
+const newCohort = ({
+  filters = {},
+  customName,
+}: {
+  filters?: IndexedFilterSet;
+  customName?: string;
+}): Cohort => {
+  const ts = new Date();
+  const newName = customName ?? defaultCohortNameGenerator();
+  const newId = createCohortId();
+  return {
+    name: newName,
+    id: newId,
+    filters: filters ?? {},
+    modified: false,
+    saved: false,
+    modified_datetime: ts.toISOString(),
+  };
+};
+
+export const createCohortId = (): string => nanoid();
+
 // TODO: start using this adapter
-/*
-const cohortsAdapter = createEntityAdapter<Cohort>({
+
+const cohortsAdapter = createEntityAdapter<Cohort, CohortId>({
   sortComparer: (a, b) => {
     if (a.modified_datetime <= b.modified_datetime) return 1;
     else return -1;
   },
+  selectId: (cohort: Cohort) => cohort.id,
 });
-*/
+
+const emptyInitialState = cohortsAdapter.getInitialState<CurrentCohortState>({
+  currentCohort: undefined,
+  message: undefined, // message is used to inform frontend components of changes to the cohort.
+});
+
+const getCurrentCohort = (
+  state: EntityState<Cohort, string> & CurrentCohortState,
+): CohortId => {
+  if (state.currentCohort) {
+    return state.currentCohort;
+  }
+
+  const unsavedCohort = newCohort({ customName: UNSAVED_COHORT_NAME });
+  return unsavedCohort.id;
+};
 
 /**
  * Redux slice for cohort filters
@@ -54,107 +119,211 @@ const cohortsAdapter = createEntityAdapter<Cohort>({
 
 export const cohortSlice = createSlice({
   name: 'cohort',
-  initialState: initialCohortState,
+  initialState: emptyInitialState,
   reducers: {
-    // adds a filter to the cohort filter set at the given index
-    updateCohortFilter: (
-      state: Draft<CohortState>,
-      action: PayloadAction<UpdateFilterParams>,
+    addNewDefaultUnsavedCohort: (
+      state,
+      action: PayloadAction<{
+        name?: string;
+      }>,
     ) => {
+      const cohort = newCohort({
+        customName: action.payload.name ?? UNSAVED_COHORT_NAME,
+      });
+      cohortsAdapter.addOne(state, cohort);
+      state.currentCohort = cohort.id;
+      state.message = [`newCohort|${cohort.name}|${cohort.id}`];
+    },
+    updateCohortName: (state, action: PayloadAction<string>) => {
+      cohortsAdapter.updateOne(state, {
+        id: getCurrentCohort(state),
+        changes: {
+          name: action.payload,
+          modified: true,
+          modified_datetime: new Date().toISOString(),
+        },
+      });
+    },
+    removeCohort: (
+      state,
+      action: PayloadAction<{
+        shouldShowMessage?: boolean;
+        id?: string;
+      }>,
+    ) => {
+      const removedCohort =
+        state.entities[action?.payload?.id || getCurrentCohort(state)];
+      cohortsAdapter.removeOne(
+        state,
+        action?.payload?.id || getCurrentCohort(state),
+      );
+      if (action?.payload.shouldShowMessage) {
+        state.message = [
+          `deleteCohort|${removedCohort?.name}|${state.currentCohort}`,
+        ];
+      }
+    },
+    // adds a filter to the cohort filter set at the given index
+    updateCohortFilter: (state, action: PayloadAction<UpdateFilterParams>) => {
       const { index, field, filter } = action.payload;
-      return {
-        cohort: {
-          ...state.cohort,
+      const currentCohortId = getCurrentCohort(state);
+
+      if (!state.entities[currentCohortId]) {
+        return;
+      }
+
+      cohortsAdapter.updateOne(state, {
+        id: currentCohortId,
+        changes: {
           filters: {
-            ...state.cohort.filters,
+            ...state.entities[currentCohortId].filters,
             [index]: {
-              mode: state.cohort.filters?.[index]?.mode ?? 'and',
+              mode:
+                state.entities[currentCohortId]?.filters[index]?.mode ?? 'and',
               root: {
-                ...(state.cohort.filters?.[index]?.root ?? {}),
+                ...(state.entities[currentCohortId]?.filters[index]?.root ??
+                  {}),
                 [field]: filter,
               },
             },
           },
+          modified: true,
+          modified_datetime: new Date().toISOString(),
         },
-      };
+      });
     },
     // removes a filter to the cohort filter set at the given index
-    removeCohortFilter: (
-      state: Draft<CohortState>,
-      action: PayloadAction<RemoveFilterParams>,
-    ) => {
+    removeCohortFilter: (state, action: PayloadAction<RemoveFilterParams>) => {
       const { index, field } = action.payload;
-      const filters = state.cohort?.filters?.[index]?.root;
+      const currentCohortId = getCurrentCohort(state);
+
+      if (!state.entities[currentCohortId]) {
+        return;
+      }
+      const filters = state.entities[currentCohortId]?.filters[index]?.root;
       if (!filters) {
         return;
       }
       const { [field]: _a, ...updated } = filters;
-      return {
-        cohort: {
-          ...state.cohort,
+
+      cohortsAdapter.updateOne(state, {
+        id: currentCohortId,
+        changes: {
           filters: {
-            ...state.cohort.filters,
+            ...state.entities[currentCohortId]?.filters,
             [index]: {
-              mode: state.cohort.filters[index].mode,
+              mode: state.entities[currentCohortId].filters[index].mode,
               root: updated,
             },
           },
+          modified: true,
+          modified_datetime: new Date().toISOString(),
         },
-      };
+      });
     },
     // removes all filters from the cohort filter set at the given index
     clearCohortFilters: (
-      state: Draft<CohortState>,
+      state,
       action: PayloadAction<ClearAllFilterParams>,
     ) => {
       const { index } = action.payload;
-      return {
-        cohort: {
-          ...state.cohort,
+      const currentCohortId = getCurrentCohort(state);
+      if (!state.entities[currentCohortId]) {
+        return;
+      }
+      const filters = state.entities[currentCohortId]?.filters[index]?.root;
+      if (!filters) {
+        return;
+      }
+
+      cohortsAdapter.updateOne(state, {
+        id: currentCohortId,
+        changes: {
           filters: {
-            ...state.cohort.filters,
+            ...state.entities[currentCohortId]?.filters,
             [index]: {
-              // empty filter set
               mode: 'and',
               root: {},
-            } as FilterSet,
+            },
           },
+          modified: true,
+          modified_datetime: new Date().toISOString(),
         },
-      };
+      });
     },
   },
 });
 
+/**
+ * Returns the selectors for the cohorts EntityAdapter
+ * @param state - the CoreState
+ *
+ * @hidden
+ */
+export const cohortSelectors = cohortsAdapter.getSelectors(
+  (state: CoreState) => state.cohort.availableCohorts,
+);
+
+const getCurrentCohortFromCoreState = (state: CoreState): CohortId => {
+  if (state.cohort.availableCohorts.currentCohort) {
+    return state.cohort.availableCohorts.currentCohort;
+  }
+  const unsavedCohort = newCohort({ customName: UNSAVED_COHORT_NAME });
+  return unsavedCohort.id;
+};
+
 // Filter actions: addFilter, removeFilter, updateFilter
-export const { updateCohortFilter, removeCohortFilter, clearCohortFilters } =
-  cohortSlice.actions;
+export const {
+  updateCohortFilter,
+  removeCohortFilter,
+  clearCohortFilters,
+  removeCohort,
+  addNewDefaultUnsavedCohort,
+} = cohortSlice.actions;
 
-export const selectCohortFilters = (state: CoreState): IndexedFilterSet =>
-  state.cohorts.cohort.filters;
+export const selectCohortFilters = (state: CoreState): IndexedFilterSet => {
+  const currentCohortId = getCurrentCohortFromCoreState(state);
+  return state.entities[currentCohortId]?.filters;
+};
 
-export const selectCurrentCohortId = (state: CoreState): string =>
-  state.cohorts.cohort.id;
+export const selectCurrentCohortId = (state: CoreState): CohortId => {
+  return getCurrentCohort(state.cohorts);
+};
 
 export const selectCurrentCohort = (state: CoreState): Cohort =>
-  state.cohorts.cohort;
+  cohortSelectors.selectById(state, getCurrentCohortFromCoreState(state));
 
 export const selectCurrentCohortName = (state: CoreState): string =>
-  state.cohorts.cohort.name;
+  cohortSelectors.selectById(state, getCurrentCohortFromCoreState(state)).name;
+
+/**
+ * Returns the cohort's name given the id
+ * @param state - the CoreState
+ * @param cohortId - the cohort id
+ * @category Cohort
+ * @category Selectors
+ */
+export const selectCohortNameById = (
+  state: CoreState,
+  cohortId: string,
+): string | undefined => {
+  const cohort = cohortSelectors.selectById(state, cohortId);
+  return cohort?.name;
+};
 
 /**
  * Select a filter by its name from the current cohort. If the filter is not found
  * returns undefined.
  * @param state - Core
- * @param index which cohort index to select from
  * @param name name of the filter to select
  */
-export const selectIndexedFilterByName = (
+export const selectAvailableCohortByName = (
   state: CoreState,
-  index: string,
   name: string,
-): Operation | undefined => {
-  return state.cohorts.cohort.filters[index]?.root[name];
-};
+): Cohort | undefined =>
+  cohortSelectors
+    .selectAll(state)
+    .find((cohort: Cohort) => cohort.name === name);
 
 const EmptyFilterSet: FilterSet = { mode: 'and', root: {} };
 /**
@@ -167,7 +336,7 @@ export const selectIndexFilters = (
   state: CoreState,
   index: string,
 ): FilterSet => {
-  return state.cohorts.cohort.filters?.[index] ?? EmptyFilterSet; // TODO: check if this is undefined
+  return selectCurrentCohort(state).filters?.[index] ?? EmptyFilterSet; // TODO: check if this is undefined
 };
 
 export const cohortReducer = cohortSlice.reducer;
