7e572b5b418c95ca7bfe44345531caeba1f99ab6
Shawn O'Connor <ocshawn@ocshawn.com>
2025-01-24 12:10:11 -0600

    (GFF-223): Implement Cohort Discovery Start Page (#303)
    * GFF-223: Implement Cohort Discovery Start Page
    
    * GFF-224: Implement Cohort Discovery Left Side Navigation Panel and style updates
    
    ---------
    
    Co-authored-by: Shawn O'Connor <shawnoconnor@uchicago.edu>

--- packages/frontend/src/features/CohortDiscovery/types.ts
+++ packages/frontend/src/features/CohortDiscovery/types.ts
@@ -1,14 +1,36 @@
 import { Gen3AppConfigData } from '../../lib/content/types';
 import { DataTypeConfig, TabConfig } from '../CohortBuilder/types';
 
+interface EmptySelection {
+  image: string;
+  imageAlt: string;
+  title?: string;
+  subHead?: string;
+}
+
+interface LeftNavItem {
+  image: string;
+  imageAlt: string;
+  title: string;
+}
+
+interface LeftNav {
+  build: LeftNavItem;
+  saved: LeftNavItem;
+  rquest: LeftNavItem;
+}
+
 export interface CohortDiscoveryGroup {
   readonly dataConfig: DataTypeConfig; // database config
   readonly tabTitle: string; // title of the tab
   readonly tabs: ReadonlyArray<TabConfig>; // filters for the fields
+  readonly emptySelection: EmptySelection; // What to show when no filters are selected
 }
 
 export interface CohortDiscoveryConfig extends Gen3AppConfigData {
   dataIndexes: Array<CohortDiscoveryGroup>;
+  emptySelection: EmptySelection;
+  leftNav: LeftNav;
 }
 
 export type SupportedFacetTypes = 'enum';
