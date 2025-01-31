bb55482a509a7adaad5ccfe7ef32c2588a3eb440
craigrbarnes <craig.barnes@intelligentlight.com>
2025-01-31 16:51:21 -0600

    update package

--- packages/core/setupTests.ts
+++ packages/core/setupTests.ts
@@ -1,4 +1,23 @@
-import { loadEnvConfig } from "@next/env";
+import { loadEnvConfig } from '@next/env';
+
+import { server } from './test/node';
+
+beforeAll(() => {
+  // Enable API mocking before all the tests.
+  server.listen();
+});
+
+afterEach(() => {
+  // Reset the request handlers between each test.
+  // This way the handlers we add on a per-test basis
+  // do not leak to other, irrelevant tests.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Finally, disable API mocking after the tests are done.
+  server.close();
+});
 
 export default async () => {
   const projectDir = process.cwd();
