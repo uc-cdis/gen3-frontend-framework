13822b37b83e0d3155588f44b9e3b325ee705e21
craigrbarnes <craig.barnes@intelligentlight.com>
2025-03-31 11:30:08 -0500

    update start.sh echo

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ GEN3_FRONTEND_CONFIGURATION_ROOT=${GEN3_FRONTEND_CONFIGURATION_ROOT:-"./config"}
 
 set -e
 export NODE_ENV=production
-echo "npm run start"
+echo "running NextJS server"
 HOSTNAME=0.0.0.0
 node server.js
