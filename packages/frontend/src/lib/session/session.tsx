cbb4f784d454d58c4eb6393cb5fc5f1fef0d035a
Craig Barnes <craig.barnes@intelligentlight.com>
2025-04-14 17:15:16 -0500

    Chore/0.10.90 b (#385)
    * add useCSRF to Session
    
    * v0.10.90
    
    * 0.10.91
    
    * manually bump lerna patch numver
    
    * v0.10.91

--- packages/frontend/src/lib/session/session.tsx
+++ packages/frontend/src/lib/session/session.tsx
@@ -21,6 +21,7 @@ import {
   showModal,
   useCoreDispatch,
   useCoreSelector,
+  useGetCSRFQuery,
   useLazyFetchUserDetailsQuery,
 } from '@gen3/core';
 
@@ -175,7 +176,7 @@ export const SessionProvider = ({
 }: SessionProviderProps) => {
   const router = useRouter();
   const coreDispatch = useCoreDispatch();
-
+  useGetCSRFQuery();
   useWorkspaceResourceMonitor(monitorWorkspace); // monitor workspaces if any are running or configured
 
   const [getUserDetails] = useLazyFetchUserDetailsQuery(); // Fetch user details
