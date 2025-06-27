3993c283eb2d878715ddaa37c5b86ae428cc1c0a
craigrbarnes <craig.barnes@intelligentlight.com>
2025-06-27 11:36:26 -0500

    add filterConverter

--- packages/tools/src/gdcGqlToGuppyGql/convert.ts
+++ packages/tools/src/gdcGqlToGuppyGql/convert.ts
@@ -2,23 +2,29 @@ import { Args, getArgs } from '../utils';
 import { convertGDCFilterToGen3Filter } from './converter';
 
 interface ConverterArgs extends Args {
-  query?: string;
+  query: string;
 }
 
-const { query  }: ConverterArgs = getArgs({
-  query: undefined,
-});
 
 
-const main = () => {
 
-  if (!query) {
-    console.log('No query provided');
-    return;
-  }
+const main = async () => {
+  console.log('Starting');
   try {
+
+    const { query  }: ConverterArgs = getArgs({
+      query: "",
+    });
+
+    if (!query) {
+      console.log('No query provided');
+      return;
+    }
+
+    console.log("query",  query);
     const gdcQuery = JSON.parse(query);
 
+    console.log(gdcQuery);
     const gen3Query = convertGDCFilterToGen3Filter(gdcQuery);
     console.log(gen3Query);
 
@@ -27,3 +33,5 @@ const main = () => {
   }
 
 }
+
+main();
