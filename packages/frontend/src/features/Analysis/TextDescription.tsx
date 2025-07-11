c7931ef82d6b234f67f026558f274b7875352623
Shawn O'Connor <ocshawn@ocshawn.com>
2025-07-11 14:46:21 -0500

    GFF-361 GFF-362: Fix card layout in Firefox Fix extend text rendering (#430)
    Co-authored-by: Shawn O'Connor <shawnoconnor@uchicago.edu>

--- packages/frontend/src/features/Analysis/TextDescription.tsx
+++ packages/frontend/src/features/Analysis/TextDescription.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState, ReactElement } from 'react';
-import { Text } from '@mantine/core';
+import React, { ReactElement } from 'react';
+import { Spoiler } from '@mantine/core';
 import {
   MdKeyboardArrowDown as DownArrowIcon,
   MdKeyboardArrowUp as UpArrowIcon,
@@ -12,46 +12,14 @@ interface TextDescriptionProps {
 const TextDescription = ({
   description,
 }: TextDescriptionProps): ReactElement => {
-  const [extend, setExtend] = useState(false);
-  const textRef = useRef<HTMLParagraphElement>(null);
-  const [isClamped, setClamped] = useState(false);
-
-  useEffect(() => {
-    // Function that should be called on window resize
-    function handleResize() {
-      if (textRef && textRef.current) {
-        setClamped(textRef.current.scrollHeight > textRef.current.clientHeight);
-      }
-    }
-
-    // Add event listener to window resize
-    window.addEventListener('resize', handleResize);
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize);
-  }, []); // Empty array ensures that it would only run on mount
-
   return (
-    <>
-      {!extend ? (
-        <Text ref={textRef} size="sm" lineClamp={4}>
-          {description}
-        </Text>
-      ) : (
-        <Text size="sm">{description}</Text>
-      )}
-      {isClamped && (
-        <button onClick={() => setExtend((extend) => !extend)}>
-          <div className="flex text-xs font-semibold text-base-contrast-light">
-            {!extend ? 'Extend Text' : 'Collapse Text'}{' '}
-            <div className="my-auto">
-              {!extend ? <DownArrowIcon /> : <UpArrowIcon />}
-            </div>
-          </div>
-        </button>
-      )}
-    </>
+    <Spoiler
+      maxHeight={80}
+      showLabel={<span>Extend Text <DownArrowIcon className="inline"/></span>}
+      hideLabel={<span>Collapse Text <UpArrowIcon className="inline" /></span>}
+    >
+      {description}
+    </Spoiler>
   );
 };
-
 export default TextDescription;
