c312962b9c996f750c8b583e077ddb5aa5e75f5d
Stephen Dranger <stephen@occ-data.org>
2025-07-17 13:43:19 -0500

    pop up display even if no data found

--- packages/frontend/src/features/CohortBuilder/ExplorerTable/ExploreTableDetails/QueryRowDetailsPanel.tsx
+++ packages/frontend/src/features/CohortBuilder/ExplorerTable/ExploreTableDetails/QueryRowDetailsPanel.tsx
@@ -33,6 +33,10 @@ const ExtractData = (
   let rowData = data.data[index][0];
   if (path) {
     const tmp = JSONPath({ path: path, json: data.data[index][0] });
+    if (!isArray(tmp)) {
+      console.log(path, data);
+      return {};
+    }
     if (tmp.length > 0) {
       rowData = tmp[0];
     }
