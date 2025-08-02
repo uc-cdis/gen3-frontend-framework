8480e06b227272d105a9e04466fe19e28d470745
Craig Barnes <craig.barnes@intelligentlight.com>
2025-08-02 11:49:13 -0500

    add SegmentControlStory (#468)

---
+++ packages/frontend/src/components/SegmentedControl.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { BarChartIcon, SurvivalChartIcon } from '../types/icons';
+import { SegmentedControlItem, Tooltip } from '@mantine/core';
+import SegmentedControl from './SegmentedControl';
+
+const chartButtons: SegmentedControlItem[] = [
+  {
+    value: 'histogram',
+    label: (
+      <Tooltip label="Histogram" position="bottom-end" withArrow arrowSize={7}>
+        <div
+          data-testid="button-histogram-plot"
+          role="button"
+          aria-label={`Select test histogram plot`}
+        >
+          <BarChartIcon size={20} aria-hidden="true" />
+        </div>
+      </Tooltip>
+    ),
+  },
+  {
+    value: 'survival',
+    label: (
+      <Tooltip label={'Survival Plot'} withArrow arrowSize={7}>
+        <div
+          data-testid="button-survival-plot"
+          role="button"
+          aria-label={`Select test survival plot`}
+        >
+          <SurvivalChartIcon size={20} aria-hidden="true" />
+        </div>
+      </Tooltip>
+    ),
+  },
+];
+
+const meta = {
+  component: SegmentedControl,
+  decorators: [
+    (Story) => (
+      <div className="bg-base-lightest p-4">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof SegmentedControl>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    padding: 1,
+    data: chartButtons,
+  },
+};
--- packages/frontend/src/types/icons.ts
+++ packages/frontend/src/types/icons.ts
@@ -1,6 +1,7 @@
 /// Icons that are part of the Gen3 FEF and not are not configurable.
 import {
   MdAdd as AddIcon,
+  MdBarChart as BarChartIcon,
   MdClose as CloseIcon,
   MdContentCopy as CopyIcon,
   MdDelete as DeleteIcon,
@@ -8,6 +9,7 @@ import {
   MdFileUpload as UploadIcon,
   MdLibraryAdd as AddFacetIcon,
   MdLockOutline as LockOutlineIcon,
+  MdTrendingDown as SurvivalChartIcon,
   MdWarning as WarningTriangleIcon,
 } from 'react-icons/md';
 import { RiErrorWarningFill as WarningMessageIcon } from 'react-icons/ri';
@@ -16,13 +18,15 @@ import { FaCircle as CircleIcon, FaPlus as PlusIcon } from 'react-icons/fa';
 export {
   AddIcon,
   AddFacetIcon,
-  DeleteIcon,
-  PlusIcon,
-  LockOutlineIcon,
+  BarChartIcon,
   CircleIcon,
   CloseIcon,
   CopyIcon,
   DownloadIcon,
+  DeleteIcon,
+  LockOutlineIcon,
+  PlusIcon,
+  SurvivalChartIcon,
   UploadIcon,
   WarningTriangleIcon,
   WarningMessageIcon,
