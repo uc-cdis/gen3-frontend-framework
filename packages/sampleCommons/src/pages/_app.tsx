f1b61e439ff857aa1c92c95dc0e6d8e5bf69b39c
craigrbarnes <craig.barnes@intelligentlight.com>
2025-08-21 18:46:33 -0500

    restore css imports

--- packages/sampleCommons/src/pages/_app.tsx
+++ packages/sampleCommons/src/pages/_app.tsx
@@ -21,7 +21,6 @@ import { registerCohortTableCustomCellRenderers } from '@/lib/CohortBuilder/Cust
 import { registerCustomExplorerDetailsPanels } from '@/lib/CohortBuilder/FileDetailsPanel';
 
 import '../styles/globals.css';
-import 'graphiql/style.css';
 import '@fontsource/montserrat';
 import '@fontsource/source-sans-pro';
 import '@fontsource/poppins';
