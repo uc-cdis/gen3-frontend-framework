a3f0053ca0172442ab3dcc1873f937f6de645e65
craigrbarnes <craig.barnes@intelligentlight.com>
2025-09-06 16:12:35 -0500

    fix tailwind issue

--- packages/sampleCommons/tailwind.config.js
+++ packages/sampleCommons/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/features/**/*.{js,ts,jsx,tsx}',
-    '../frontend/dist/**/index.js',
+    '../frontend/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
