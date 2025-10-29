db5daf8cdbcf492ac2696de0bb3db96b72915516
Shawn O'Connor <shawnoconnor@uchicago.edu>
2025-10-29 16:01:07 -0500

    GFF-397: put back graph zooming and proper type

--- packages/frontend/src/features/Dictionary/GraphView.tsx
+++ packages/frontend/src/features/Dictionary/GraphView.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import ReactECharts, { ReactEChartsProps } from '../../components/charts/echarts/ReactECharts';
-import type { TreeSeriesOption } from 'echarts';
+import type { GraphSeriesOption } from 'echarts';
 import { formatDataForGraph } from './graphViewDataUtils';
 import { DataDictionary } from './types';
 
@@ -15,16 +15,28 @@ const GraphView = ({
   dictionary,
   selectedId,
 }: GraphViewType) => {
-  const [zoomLevel, setZoomLevel] = useState(0);
+  const [zoomLevel, setZoomLevel] = useState(1);
   const dataAndLinks = formatDataForGraph(dictionary);
   console.log('GraphView dictionary', dictionary);
   console.log('GraphView categories', categories);
   console.log('GraphView selectedId', selectedId);
-  interface TreeReactEChartsProps extends ReactEChartsProps {
+  interface GraphReactEChartsProps extends ReactEChartsProps {
     option: {
-      series: TreeSeriesOption[];
+      series: GraphSeriesOption[];
     };
   }
+
+  //TODO list
+  //position labels
+  //line strait up and down
+  //curves at end of line 
+  //positioning of nodes
+  //positioning of lines
+  //icones
+  //icon images
+  //line color
+  //lines that curve back in last row
+  //hover upstreem only emphasis
 
   const chartDefinition = useMemo((): ReactEChartsProps['option'] => {
     return {
@@ -58,7 +70,7 @@ const GraphView = ({
             show: true,
             title: 'Zoom In',
             icon: 'image://data:image/gif;base64,R0lGODlhHgAeAKEAADKDyP///zKDyDKDyCH5BAEKAAIALAAAAAAeAB4AAAJAhI+py+0PIww0yFnvq1Y37n1ZuICkYp5Iqhps+27cTNfdauc0qvcU79NpYiciyRhCepTDUcvlfDIvU0n1ic2GCgA7',
-            onclick: (params: TreeReactEChartsProps) => {
+            onclick: (params: GraphReactEChartsProps) => {
               const series = params?.option?.series;
               const zoom = series && series[0] ? series[0].zoom : 1;
 
@@ -69,7 +81,7 @@ const GraphView = ({
             show: true,
             title: 'Zoom Out',
             icon: 'image://data:image/gif;base64,R0lGODlhHgAeAKEAADKDyP///zKDyDKDyCH5BAEKAAIALAAAAAAeAB4AAAIohI+py+0Po5y02ouz3rz7PwXiSJbmqJzqmq4u2b4vSNf2jef6zvd1AQA7',
-            onclick: (params: TreeReactEChartsProps) => {
+            onclick: (params: GraphReactEChartsProps) => {
               const series = params?.option?.series;
               const zoom = series && series[0] ? series[0].zoom : 1;
 
@@ -83,18 +95,23 @@ const GraphView = ({
         type: 'graph',
         roam: true,
         zoom: zoomLevel,
-        label: { //TODO lable size stay same on zoom
+        nodeScaleRatio: 1 as 0.6,//Temp type fix untill its fixed in echarts
+        label: {
           show: true,
           position: 'right',
+          align: 'left',
+          verticalAlign: 'middle',
           backgroundColor: 'white',
-          fontSize: 12,
+          fontSize: 12 * zoomLevel, //have label size increase with zoom
         },
-        symbol: 'circle',
+        symbol: 'circle', //TODO make image
+        symbolSize: 15,
+        symbolOffset: ['-50%', 0],
         emphasis: {
           focus: 'adjacency'
         },
-        //edgeSymbol: ['circle', 'circle'],
-        //edgeSymbolSize: [4, 4],
+        edgeSymbol: ['circle', 'circle'],
+        edgeSymbolSize: 6 * zoomLevel, //have edge size increase with zoom
         lineStyle: {
           opacity: 0.9,
           width: 2,
@@ -117,7 +134,18 @@ const GraphView = ({
 //TODO: ZOOM IN AND OUT AND MOVE AROUND controls? 
   return (
     <div className="w-full h-full min-h-[800px]">
-      <ReactECharts option={chartDefinition} />
+      <ReactECharts option={chartDefinition} events={{
+        graphroam: function () {
+          //getting zoom from event does not work, so get it from chart options
+          const thisApi = this._api as unknown as { getOption: () => any };
+          const thisOptions: GraphReactEChartsProps['option'] = thisApi.getOption();
+          const seriesGraph = thisOptions?.series && thisOptions.series[0];
+          const seriesGraphZoom = seriesGraph?.zoom;
+          if (seriesGraphZoom){
+            setZoomLevel(seriesGraphZoom);
+          }
+        },
+      }} />
     </div>
   );
 };
