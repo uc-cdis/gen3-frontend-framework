7784a3d2fa2129155e7b1e4d27bc5ed1555dccb1
craigrbarnes <craig.barnes@intelligentlight.com>
2026-01-22 10:06:46 -0600

    updated middleware

--- packages/sampleCommons/src/middleware.ts
+++ packages/sampleCommons/src/middleware.ts
@@ -3,9 +3,5 @@
 export { middleware } from './middleware-impl';
 
 export const config = {
-  matcher: [
-  "/DataLibrary",
-  "/Workspace",
-  "/Profile"
-],
+  matcher: ['/DataLibrary', '/Workspace', '/Profile'],
 };
