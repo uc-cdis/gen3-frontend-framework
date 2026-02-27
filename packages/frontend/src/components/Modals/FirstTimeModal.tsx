2845f8db9068c1701a588df0a253e44da50d53fd
Paribartan Dhakal <paribartandhakal@macbookpro.mynetworksettings.com>
2026-02-27 09:27:27 -0500

    use modal context

--- packages/frontend/src/components/Modals/FirstTimeModal.tsx
+++ packages/frontend/src/components/Modals/FirstTimeModal.tsx
@@ -1,53 +1,44 @@
 import React from 'react';
-import { Text } from '@mantine/core';
+import { ContextModalProps } from '@mantine/modals';
+import { Button } from '@mantine/core';
 import { useCookies } from 'react-cookie';
-import { BaseModal } from './BaseModal';
 import TextContent from '../Content/TextContent';
 import { FirstTimeModalConfig } from './types';
 
-interface FirstTimeModalProps {
-  readonly openModal: boolean;
+export interface FirstTimeModalProps {
   config: FirstTimeModalConfig;
 }
 
 export const FirstTimeModal = ({
-  openModal,
-  config,
-}: FirstTimeModalProps): JSX.Element => {
+  context,
+  id,
+  innerProps,
+}: ContextModalProps<FirstTimeModalProps>) => {
+  const { config } = innerProps;
   const [cookie, setCookie] = useCookies(['Gen3-first-time-use']);
 
   const handleAccept = () => {
-    const isFirstTimeUse = cookie['Gen3-first-time-use'];
-    if (!isFirstTimeUse) {
+    if (!cookie['Gen3-first-time-use']) {
       const maxAge = 60 * 60 * 24 * (config?.expireDays ?? 365);
       setCookie('Gen3-first-time-use', true, { maxAge });
     }
+    context.closeModal(id);
   };
 
   return (
-    <BaseModal
-      title={
-        <Text size="lg" className="font-medium font-heading">
-          {config.title ?? 'Welcome to Gen3'}
-        </Text>
-      }
-      openModal={openModal}
-      size="60%"
-      buttons={[
-        {
-          title: 'Accept',
-          onClick: handleAccept,
-          hideModalOnClick: true,
-          dataTestId: 'button-intro-warning-accept',
-        },
-      ]}
-      withCloseButton={false}
-      closeOnClickOutside={false}
-      closeOnEscape={false}
-    >
-      <div className="border-y border-y-base-darker py-4 space-y-4 font-content">
-        <TextContent key={'first_time_use_modal'} {...config.content} />
+    <>
+      <div className="border-y border-y-base-darker p-4 space-y-4 font-content">
+        <TextContent key="first_time_use_modal" {...config.content} />
       </div>
-    </BaseModal>
+      <div className="flex justify-end mt-2.5 gap-2 p-4">
+        <Button
+          data-testid="button-intro-warning-accept"
+          onClick={handleAccept}
+          className="!bg-primary hover:!bg-primary-darker"
+        >
+          Accept
+        </Button>
+      </div>
+    </>
   );
 };
--- packages/frontend/src/components/Modals/registerModals.ts
+++ packages/frontend/src/components/Modals/registerModals.ts
@@ -1,7 +1,9 @@
 import BaseContextModal from './BaseContextModal';
 import { SaveCohortModal } from './SaveCohortModal';
+import { FirstTimeModal } from './FirstTimeModal';
 
 export const gen3Modals = {
   baseContextModal: BaseContextModal,
   saveCohortModal: SaveCohortModal,
+  firstTimeModal: FirstTimeModal,
 };
