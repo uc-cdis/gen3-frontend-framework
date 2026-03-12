fd349a653110db70d78f80e9bcbb2f4785dc40d4
Amy Lehman <amlehman77@gmail.com>
2026-03-12 14:18:24 -0500

    GFF-202 - add loading state for cards

--- packages/frontend/src/features/Sower/JobOverview.tsx
+++ packages/frontend/src/features/Sower/JobOverview.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Paper } from '@mantine/core';
-import { JobListResponse } from '@gen3/core';
+import { Paper, Loader } from '@mantine/core';
 import { FaExclamationTriangle as FailedIcon } from 'react-icons/fa';
 import { LuClock as CompletedIcon } from 'react-icons/lu';
 import { FiActivity as ActiveIcon } from 'react-icons/fi';
 import { IconBaseProps } from 'react-icons';
+import { JobListResponse } from '@gen3/core';
 
 interface JobOverviewCardProps {
   readonly Icon: React.FC<IconBaseProps>;
   readonly color: string;
   readonly count: number;
   readonly text: string;
+  readonly isLoading: boolean;
 }
 
 const JobOverviewCard = ({
@@ -18,6 +19,7 @@ const JobOverviewCard = ({
   color,
   count,
   text,
+  isLoading,
 }: JobOverviewCardProps) => {
   return (
     <Paper
@@ -32,7 +34,7 @@ const JobOverviewCard = ({
         <Icon size={32} className={`text-${color}`} />
       </div>
       <div className="flex flex-col">
-        <p className="text-2xl font-bold">{count}</p>
+        {isLoading ? <Loader type="dots" /> : <p className="text-2xl font-bold">{count}</p>}
         {text}
       </div>
     </Paper>
@@ -44,7 +46,7 @@ interface JobOverviewProps {
   readonly isLoading: boolean;
 }
 
-const JobOverview = ({ data }: JobOverviewProps) => {
+const JobOverview = ({ data, isLoading }: JobOverviewProps) => {
   const groupedData = data ? Object.groupBy(data, (row) => row.status) : {};
 
   return (
@@ -54,18 +56,21 @@ const JobOverview = ({ data }: JobOverviewProps) => {
         count={groupedData?.Running?.length || 0}
         text="Active Jobs"
         color="utility-success"
+        isLoading={isLoading}
       />
       <JobOverviewCard
         Icon={CompletedIcon}
         count={groupedData?.Completed?.length || 0}
         text="Completed"
         color="utility-success"
+        isLoading={isLoading}
       />
       <JobOverviewCard
         Icon={FailedIcon}
         count={groupedData?.Failed?.length || 0}
         text="Failed"
         color="utility-error"
+        isLoading={isLoading}
       />
     </div>
   );
