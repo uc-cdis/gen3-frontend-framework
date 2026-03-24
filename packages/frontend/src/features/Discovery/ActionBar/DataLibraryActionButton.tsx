e3dcf056b6354cdc2be0133140ca664ba8001cb9
Amy Lehman <amlehman77@gmail.com>
2026-03-24 10:10:19 -0500

    GFF-544 - remove forward refs

--- packages/frontend/src/features/Discovery/ActionBar/DataLibraryActionButton.tsx
+++ packages/frontend/src/features/Discovery/ActionBar/DataLibraryActionButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, forwardRef } from 'react';
+import React, { ReactNode } from 'react';
 import { Button, ButtonProps, Tooltip, Loader } from '@mantine/core';
 import { FiDownload as DownloadIcon } from 'react-icons/fi';
 import { useIsUserLoggedIn } from '@gen3/core';
@@ -12,51 +12,45 @@ export interface ExportActionButtonProps {
   showIcon?: boolean;
   loginRequired?: boolean;
   onClick?: (items: Record<string, any> | Array<any>) => void;
+  ref: React.RefObject<HTMLButtonElement>;
 }
 
-const DataLibraryActionButton = forwardRef<
-  HTMLButtonElement,
-  ExportActionButtonProps & ButtonProps
->(
-  (
-    {
-      label = undefined,
-      icon = undefined,
-      disabled = false,
-      tooltip = undefined,
-      onClick = () => null,
-      active = false,
-      showIcon = true,
-      loginRequired = false,
-      ...buttonProps
-    }: ExportActionButtonProps & ButtonProps,
+const DataLibraryActionButton = (
+  {
     ref,
-  ) => {
-    // TODO Test what idp was used to login and restrict actions to that idp or all or none
-    const requiresLogin = !useIsUserLoggedIn() && loginRequired;
+    label = undefined,
+    icon = undefined,
+    disabled = false,
+    tooltip = undefined,
+    onClick = () => null,
+    active = false,
+    showIcon = true,
+    loginRequired = false,
+    ...buttonProps
+  }: ExportActionButtonProps & ButtonProps
+) => {
+  // TODO Test what idp was used to login and restrict actions to that idp or all or none
+  const requiresLogin = !useIsUserLoggedIn() && loginRequired;
 
-    const buttonIcon = active ? (
-      <Loader size="sm" className="p-1" />
-    ) : (
-      <DownloadIcon title="download" size={16} />
-    );
+  const buttonIcon = active ? (
+    <Loader size="sm" className="p-1" />
+  ) : (
+    <DownloadIcon title="download" size={16} />
+  );
 
-    return (
-      <Tooltip disabled={!tooltip} label={tooltip}>
-        <Button
-          ref={ref}
-          onClick={onClick}
-          disabled={disabled || requiresLogin}
-          leftSection={showIcon ? buttonIcon : undefined}
-          {...buttonProps}
-        >
-          {label}
-        </Button>
-      </Tooltip>
-    );
-  },
-);
-
-DataLibraryActionButton.displayName = 'DataLibraryActionButton';
+  return (
+    <Tooltip disabled={!tooltip} label={tooltip}>
+      <Button
+        ref={ref}
+        onClick={onClick}
+        disabled={disabled || requiresLogin}
+        leftSection={showIcon ? buttonIcon : undefined}
+        {...buttonProps}
+      >
+        {label}
+      </Button>
+    </Tooltip>
+  );
+};
 
 export default DataLibraryActionButton;
