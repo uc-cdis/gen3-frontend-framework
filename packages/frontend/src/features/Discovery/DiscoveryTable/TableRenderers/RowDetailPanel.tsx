d31e4cffaaacfdd7e0b92c66e56fc8ca8efb43f9
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2026-03-26 11:04:00 -0500

    Feat/discovery detail panel tags (#674)
    * feat(discoveryRepositorySelection): Updated proxy API with getTagCategoryData
    
    * feat(discoveryRepositorySelection): Added missing types
    
    * feat(discoveryRepositorySelection): Began UI for multiselect
    
    * feat(discoveryRepositorySelection): fixed button
    
    * feat(discoveryRepositorySelection): Made progress towards having user selections from dropdown pass to backup and update UI
    
    * feat(discoveryRepositorySelection): Updated handle change to update changes in real time
    
    * feat(discoveryRepositorySelection): Added custom rendered for dropdown to support colors on selection, fixed TS issues and added missing interfaces
    
    * feat(discoveryRepositorySelection): Refactored DiscoveryDropdownTagViewer to work based off of the selectedTags, still need to address user removing tags from dropdown issue
    
    * feat(discoveryRepositorySelection): Refactored DiscoveryDropdownTagViewer to maintain selectedTags array in real time including deletion of multiple tags at once
    
    * feat(discoveryRepositorySelection): styled buttons
    
    * feat(discoveryRepositorySelection): fixed bug in displaying items as active, resolved TS issues
    
    * feat(discoveryRepositorySelection): resolved TS issues
    
    * feat(discoveryRepositorySelection): Resolved more TS issues, code cleanup, renaming of files and variable to better communicate purpose
    
    * feat(discoveryRepositorySelection): Set run lint to quiet to see if CI tests will pass
    
    * feat(discoveryRepositorySelection): Set learna run lint to quiet to see if CI tests will pass
    
    * feat(discoveryRepositorySelection): Reverted changes to ci.yaml
    
    * feat(discoveryRepositorySelection): Handled loading UX
    
    * feat(discoveryRepositorySelection): fixed button style
    
    * feat(discoveryRepositorySelection): Removed linting from CI to see if fixes build issue
    
    * feat(discoveryRepositorySelection): Reverted linting after confirming it is responsible for build issue
    
    * feat(discoveryRepositorySelection): fixed lint issues
    
    * feat(discoveryRepositorySelection): Updated logic for rendering / not rendering based on config
    
    * feat(discoveryRepositorySelection): made layout more responisve to viewport size
    
    * feat(discoveryRepositorySelection): fixed space issue
    
    * feat(discoveryRepositorySelection): simplified responsive design logic
    
    * feat(discoveryRepositorySelection): made use of hex code for colors consistent
    
    * feat(discoveryRepositorySelection): Removed unneeded log statement
    
    * feat(discoveryDetailPanelTags): initial commit
    
    * feat(discoveryDetailPanelTags): Added logic to update selected Tags
    
    * feat(discoveryDetailPanelTags): Refactored to use context to decrease prop drilling throughout application
    
    * feat(discoveryDetailPanelTags): Address edge case updating bug using useRef instead of useState
    
    * feat(discoveryDetailPanelTags): General code cleanup
    
    * feat(discoveryDetailPanelTags): More general code cleanup
    
    * feat(discoveryDetailPanelTags): Removed unneeded comments
    
    * feat(discoveryDetailPanelTags): Fixed build issues
    
    * feat(discoveryDetailPanelTags): make discovery provider props consistent
    
    * feat(discoveryDetailPanelTags): Removed stray ;
    
    ---------
    
    Co-authored-by: Craig Barnes <craig.barnes@intelligentlight.com>

--- packages/frontend/src/features/Discovery/DiscoveryTable/TableRenderers/RowDetailPanel.tsx
+++ packages/frontend/src/features/Discovery/DiscoveryTable/TableRenderers/RowDetailPanel.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import { Box, Text } from '@mantine/core';
 import HighlightSearchTerm from '../SearchHighlighting/HighlightSearchTerm';
 import { MRT_Row, MRT_RowData } from 'mantine-react-table';
-import { DiscoveryIndexConfig } from '../../types';
 import _ from 'lodash';
+import RowDetailPanelTags from './RowDetailPanelTags';
+import { useDiscoveryContext } from '../../DiscoveryProvider';
 
 interface RowDetailPanelProps {
   row: MRT_Row<MRT_RowData>;
-  config: DiscoveryIndexConfig;
   searchTerm: string;
 }
 
-const RowDetailPanel = ({ row, config, searchTerm }: RowDetailPanelProps) => {
+const RowDetailPanel = ({ row, searchTerm }: RowDetailPanelProps) => {
+  const { discoveryConfig: config } = useDiscoveryContext();
   if (config.studyPreviewField) {
     const studyPreviewData = _.get(
       row.original,
       config.studyPreviewField.field,
     );
     return (
-      <Box display={'flex'} w={'100%'}>
-        <Text size="xs" lineClamp={2}>
-          {HighlightSearchTerm(studyPreviewData, searchTerm)}
-        </Text>
-      </Box>
+      <>
+        <Box display={'flex'} w={'100%'}>
+          <Text size="xs" lineClamp={2}>
+            {HighlightSearchTerm(studyPreviewData, searchTerm)}
+          </Text>
+        </Box>
+        <RowDetailPanelTags rowTags={row.original.tags} />
+      </>
     );
   } else {
     return undefined;
---
+++ packages/frontend/src/features/Discovery/DiscoveryTable/TableRenderers/RowDetailPanelTags.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Badge } from '@mantine/core';
+import { TagData } from '../../../Study';
+import { getTagInfo } from '../../../Study/utils';
+import { useDiscoveryContext } from '../../DiscoveryProvider';
+
+interface RowDetailPanelProps {
+  rowTags: TagData[];
+}
+
+const RowDetailPanelTags = ({ rowTags }: RowDetailPanelProps) => {
+  const {
+    discoveryConfig: config,
+    selectedTags,
+    setSelectedTags,
+  } = useDiscoveryContext();
+  return (
+    <div className="flex mt-2">
+      {rowTags.map((tag: TagData) => {
+        const { color, display, label } = getTagInfo(tag, config.tags);
+        if (!display) return null;
+        const active = Object.keys(selectedTags).includes(tag.name);
+
+        const handleChange = (
+          e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        ) => {
+          e.stopPropagation();
+          if (active) {
+            // If already active, remove the tag
+            setSelectedTags((prevTags) => {
+              const { [tag.name]: _, ...newTags } = prevTags;
+              return newTags;
+            });
+          } else {
+            // Otherwise add the tag
+            setSelectedTags((prevObj) => ({
+              ...prevObj,
+              [tag.name]: true,
+            }));
+          }
+        };
+        return (
+          <Badge
+            key={tag.name}
+            className="mr-2 mt-2 cursor-pointer"
+            onClick={(e) => {
+              handleChange(e);
+            }}
+            role="button"
+            size="lg"
+            radius="sm"
+            variant="filled"
+            tabIndex={0}
+            aria-label={tag.name}
+            style={{
+              backgroundColor: active ? color : 'transparent',
+              borderColor: color,
+              color: active ? '#fff' : 'inherit',
+            }}
+          >
+            {label}
+          </Badge>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RowDetailPanelTags;
