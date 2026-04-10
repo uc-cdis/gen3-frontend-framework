3f11778e7c9a0107f7ca6252b56cb3d0ef60e5b5
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-04-10 12:15:58 -0500

    feat(discoveryDetailsPlaceholders): Removed more merge markers

--- packages/frontend/src/features/Study/StudyDetails/StudyGroup.tsx
+++ packages/frontend/src/features/Study/StudyDetails/StudyGroup.tsx
@@ -36,14 +36,7 @@ const StudyGroup = ({ data, header, fields }: StudyTabGroupProps) => {
 
   return (
     <div className=" flex flex-col">
-<<<<<<< HEAD
       <div className="bg-accent-lightest w-full p-1 mb-2 mt-1">
-=======
-      <div className="bg-accent-lightest w-full p-1 mb-2">
-<<<<<<< HEAD
->>>>>>> 49ef6982 (feat(discoveryDetailsPlaceholders): Began refactoring renderers into separate files)
-=======
->>>>>>> d6ad4328 (feat(discoveryDetailsPlaceholders): Began refactoring renderers into separate files)
         <Text c="primary" fw={700}>
           {header}
         </Text>
