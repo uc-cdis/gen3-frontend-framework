5f9fd8b96bdc114e37a4aa0a3237edba52a1681b
Amy Lehman <amlehman77@gmail.com>
2026-04-24 09:41:44 -0500

    GFF-564: Implement Add Data to Project (#708)
    * GFF-564 - add submission form for adding data to project
    
    * GFF-564 - clean up
    
    * GFF-564 - linting

---
+++ packages/frontend/src/features/Submission/DictionaryPanel/DictionaryPanel.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { useGetDictionaryQuery } from '@gen3/core';
+import { Loader, Switch } from '@mantine/core';
+import { removeUnusedFieldsFromDictionaryObject } from '../../Dictionary/utils';
+import DictionaryProvider from '../../Dictionary/DictionaryProvider';
+import { DataDictionary, DictionaryConfig } from '../../Dictionary';
+import MessagePanel from '../../../components/MessagePanel';
+import SubmissionForm from './SubmissionForm';
+
+const DictionaryPanel = ({ config }: { config?: DictionaryConfig }) => {
+  const [showFormSubmission, setShowFormSubmission] = useState(false);
+
+  const toggleFormSubmission = () => setShowFormSubmission(!showFormSubmission);
+
+  const { data, isLoading, isSuccess } = useGetDictionaryQuery();
+  const [dictionary, setDictionary] = useState<DataDictionary>({});
+
+  useEffect(() => {
+    if (isSuccess) {
+      const dictionary = removeUnusedFieldsFromDictionaryObject(data);
+      setDictionary(dictionary);
+    }
+  }, [data, isSuccess]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return config ? (
+    <DictionaryProvider dictionary={dictionary} config={config}>
+      <div className="flex flex-col gap-4 p-4 w-full h-screen">
+        <button className="border-1 border-solid rounded-md border-primary text-primary p-2">
+          {'Upload Structured TSV File'}
+        </button>
+        <Switch
+          color="secondary"
+          label="Use Form Submission"
+          checked={showFormSubmission}
+          onChange={toggleFormSubmission}
+        />
+        <hr className="mt-2" />
+        {showFormSubmission ? <SubmissionForm dictionary={dictionary} /> : null}
+      </div>
+    </DictionaryProvider>
+  ) : (
+    <MessagePanel message="Dictionary config is not defined. Page disabled" />
+  );
+};
+
+export default DictionaryPanel;
