d1980366023f7c2c21715e293c269a6b91cb74b5
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2026-04-28 09:46:10 -0500

    Feat/discovery details permalink (#712)
    * feat(discoveryDetailsPermalink): initial commit
    
    * feat(discoveryDetailsPermalink): fixed permalink issue
    
    * feat(discoveryDetailsPermalink): fixed close animation
    
    * feat(discoveryDetailsPermalink): cleaned up code
    
    * feat(discoveryDetailsPermalink): formatted code for consistency
    
    * feat(discoveryDetailsPermalink): more formatting code for consistency
    
    * feat(discoveryDetailsPermalink): fixed lint issue

--- packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
+++ packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Drawer } from '@mantine/core';
 import StudyDetailsPanel from './StudyDetailsPanel';
 import { useDisclosure } from '@mantine/hooks';
@@ -7,6 +7,7 @@ import { useStudyContext } from '../StudyProvider';
 import { StudyDetailView, StudyPageConfig } from '../types';
 import { DataAuthorization } from '../../../utils';
 import StudyDetailsHeaderButtons from './StudyDetailsHeaderButtons';
+import { toString } from 'lodash';
 
 const StudyDetails = ({
   index,
@@ -19,42 +20,63 @@ const StudyDetails = ({
   simpleDetailsView?: StudyPageConfig;
   authz: DataAuthorization;
 }) => {
-  const { studyDetails } = useStudyContext();
+  const { studyDetails, setStudyDetails } = useStudyContext();
   const [opened, { open, close }] = useDisclosure(false);
+  const defaultPermaLinkValue = 'Discovery/notfound';
+  const [permalink, setPermalink] = useState(defaultPermaLinkValue);
+  const hasStudyDetails = Object.keys(studyDetails).length > 0;
+
   useEffect(() => {
-    if (Object.keys(studyDetails).length > 0) {
+    const studyId = toString(studyDetails[index]);
+    const pushUrl = (path: string) =>
+      typeof window !== 'undefined' && window.history.pushState(null, '', path);
+    if (studyId) {
+      if (opened) {
+        pushUrl(`/Discovery/${encodeURI(studyId)}`);
+        setPermalink(window?.location?.href ?? defaultPermaLinkValue);
+      } else {
+        pushUrl('/Discovery');
+        setPermalink(defaultPermaLinkValue);
+      }
+    }
+    if (opened === false) {
+      // if drawer has been shut, reset study details
+      setStudyDetails({});
+    }
+  }, [opened]);
+
+  useEffect(() => {
+    if (hasStudyDetails) {
       open();
     }
   }, [studyDetails, open]);
 
-  if (!studyDetails) {
-    return null;
-  }
-
   return (
     <Drawer.Root opened={opened} onClose={close} size="50%" position="right">
-      <Drawer.Overlay opacity={0.5} blur={4} />
-      <Drawer.Content className="pl-2">
-        <Drawer.Header>
-          <StudyDetailsHeaderButtons studyIndex={index} onClose={close} />
-        </Drawer.Header>
-        <Drawer.Body>
-          {detailView ? (
-            <StudyDetailsPanel
-              data={studyDetails ?? {}}
-              studyConfig={detailView}
-            />
-          ) : simpleDetailsView ? (
-            <SinglePageStudyDetailsPanel
-              data={studyDetails ?? {}}
-              studyConfig={simpleDetailsView}
-              authorization={authz}
-            />
-          ) : (
-            <div>Study Details Panel not configured</div>
-          )}
-        </Drawer.Body>
-      </Drawer.Content>
+      <Drawer.Overlay opacity={0.5} blur={4} />{' '}
+      {hasStudyDetails && (
+        <Drawer.Content className="pl-2">
+          <Drawer.Header>
+            <StudyDetailsHeaderButtons onClose={close} permalink={permalink} />
+          </Drawer.Header>
+          <Drawer.Body>
+            {detailView ? (
+              <StudyDetailsPanel
+                data={studyDetails ?? {}}
+                studyConfig={detailView}
+              />
+            ) : simpleDetailsView ? (
+              <SinglePageStudyDetailsPanel
+                data={studyDetails ?? {}}
+                studyConfig={simpleDetailsView}
+                authorization={authz}
+              />
+            ) : (
+              <div>Study Details Panel not configured</div>
+            )}
+          </Drawer.Body>
+        </Drawer.Content>
+      )}
     </Drawer.Root>
   );
 };
