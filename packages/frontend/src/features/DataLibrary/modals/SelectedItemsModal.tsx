b41b116acb797e9a6ae118a43f4edff8ebd333cc
Shawn O'Connor <shawnoconnor@uchicago.edu>
2026-05-05 16:32:28 -0500

    GFF-569: modal overlay is higher than the z-index for the dropdown

--- packages/frontend/src/features/DataLibrary/modals/SelectedItemsModal.tsx
+++ packages/frontend/src/features/DataLibrary/modals/SelectedItemsModal.tsx
@@ -178,7 +178,6 @@ const SelectedItemsModal: React.FC<SelectedItemsModelProps> = (props) => {
       {...props}
       closeOnEscape
       centered
-      withinPortal={false}
       title={<ModalHeader />}
       classNames={{
         header: 'm-0 p-2 min-h-10 bg-secondary',
@@ -198,6 +197,7 @@ const SelectedItemsModal: React.FC<SelectedItemsModelProps> = (props) => {
             <Select
               data={destinations}
               value={value ? value.value : null}
+              comboboxProps={{ zIndex: 500 }}
               onChange={(value, option) => {
                 setValue(option);
                 if (value) setSelectionAction(value);
