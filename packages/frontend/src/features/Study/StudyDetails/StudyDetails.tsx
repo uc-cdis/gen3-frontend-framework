a6cea107e81b4fc057cd2f2b2dc5c60f79e75382
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-05-15 16:07:33 -0500

    fix(refactorPermalink): Added logic using NEXTJS router, works but with issues retaining URL due to race condition at load

--- packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
+++ packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router'; // Or 'next/navigation' if using App Router
 import { Drawer } from '@mantine/core';
-import StudyDetailsPanel from './StudyDetailsPanel';
 import { useDisclosure } from '@mantine/hooks';
-import SinglePageStudyDetailsPanel from './SinglePageStudyDetailsPanel';
 import { useStudyContext } from '../StudyProvider';
 import { StudyDetailView, StudyPageConfig } from '../types';
 import { DataAuthorization } from '../../../utils';
 import StudyDetailsHeaderButtons from './StudyDetailsHeaderButtons';
+import StudyDetailsPanel from './StudyDetailsPanel';
+import SinglePageStudyDetailsPanel from './SinglePageStudyDetailsPanel';
 
 const StudyDetails = ({
   index,
@@ -19,63 +20,72 @@ const StudyDetails = ({
   simpleDetailsView?: StudyPageConfig;
   authz: DataAuthorization;
 }) => {
-  const { studyDetails } = useStudyContext();
+  const router = useRouter();
+  const { studyDetails, setStudyDetails } = useStudyContext();
   const [opened, { open, close }] = useDisclosure(false);
-  const defaultPermaLinkValue = 'Discovery/notfound';
+
+  const defaultPath = '/Discovery';
+  const studyId = studyDetails?.[index];
   const hasStudyDetails = Object.keys(studyDetails).length > 0;
 
   const permalink = useMemo(() => {
-    const studyId = studyDetails?.[index];
-    if (studyId) {
-      const origin = window?.location?.href ?? defaultPermaLinkValue;
-      const studyPath = `${origin}/${encodeURIComponent(studyId as string)}`;
-      // router.push(studyPath, undefined, { shallow: true });
-      return studyPath;
-    } else {
-      return defaultPermaLinkValue;
+    if (studyId && typeof window !== 'undefined') {
+      const origin = window.location.origin;
+      return `${origin}${defaultPath}/${encodeURIComponent(studyId as string)}`;
     }
-  }, [index, studyDetails]);
+    return `${origin}${defaultPath}/notfound`;
+  }, [studyId]);
 
-  // using Object.keys(studyDetails).length > 0 fires this effect correctly
+  // Sync Browser URL with Drawer state
   useEffect(() => {
-    if (Object.keys(studyDetails).length > 0) {
+    if (opened && studyId) {
+      const studyPath = `${defaultPath}/${encodeURIComponent(studyId as string)}`;
+      if (router.asPath !== studyPath) {
+        router.push(studyPath, undefined, { shallow: true });
+      }
+    }
+    if (!opened && hasStudyDetails === false) {
+      router.push(defaultPath, undefined, { shallow: true });
+    }
+  }, [opened, studyId]);
+
+  useEffect(() => {
+    if (hasStudyDetails) {
       open();
     }
-  }, [studyDetails, open]);
+  }, [hasStudyDetails, open]);
+
+  const handleClose = () => {
+    close();
+    setStudyDetails({});
+  };
 
   return (
     <Drawer.Root
       opened={opened}
-      onClose={() => {
-        close();
-      }}
+      onClose={handleClose}
       size="50%"
       position="right"
     >
-      <Drawer.Overlay opacity={0.5} blur={4} />{' '}
+      <Drawer.Overlay opacity={0.5} blur={4} />
       {hasStudyDetails && (
         <Drawer.Content className="pl-2">
           <Drawer.Header>
             <StudyDetailsHeaderButtons
-              onClose={close}
+              onClose={handleClose}
               permalink={permalink}
               showSubmitButton={simpleDetailsView?.showSubmitButton}
             />
           </Drawer.Header>
           <Drawer.Body>
             {detailView ? (
-              <StudyDetailsPanel
-                data={studyDetails ?? {}}
-                studyConfig={detailView}
-              />
-            ) : simpleDetailsView ? (
+              <StudyDetailsPanel data={studyDetails} studyConfig={detailView} />
+            ) : (
               <SinglePageStudyDetailsPanel
-                data={studyDetails ?? {}}
-                studyConfig={simpleDetailsView}
+                data={studyDetails}
+                studyConfig={simpleDetailsView!}
                 authorization={authz}
               />
-            ) : (
-              <div>Study Details Panel not configured</div>
             )}
           </Drawer.Body>
         </Drawer.Content>
