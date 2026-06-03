4edfe2aab088799a030326d4aa3eb601d11e15ea
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-06-03 14:28:44 -0500

    feat(dataAccessArborist): Implemented line by line port of code

--- packages/sampleCommons/src/utils/api/discovery/preProcessData/addAccessLevelsMetaData.ts
+++ packages/sampleCommons/src/utils/api/discovery/preProcessData/addAccessLevelsMetaData.ts
@@ -29,49 +29,104 @@ const addAccessLevelsMetaData = async (
     const userHasAccessToResource = userAuthzResources?.includes(
       authz as string,
     );
+
+    let accessible: AccessLevel = AccessLevel.OTHER;
+    /**
+     *
+     * Line by line porting from Data portal src/Discovery/index.tsx
+     *
+     */
+    // ln 151 if
+    // supportedValues?.unaccessible?.enabled && dataAvailabilityField && study[dataAvailabilityField] === 'unaccessible')
+    if (data_availability && data_availability === 'unaccessible') {
+      accessible = AccessLevel.UNACCESSIBLE;
+    }
+    // ln 155
+    // else if (supportedValues?.notAvailable?.enabled && dataAvailabilityField && study[dataAvailabilityField] === 'not_available')
+    else if (data_availability && data_availability === 'not_available') {
+      accessible = AccessLevel.NOT_AVAILABLE;
+    }
+    // ln 159
+    // else if (supportedValues?.waiting?.enabled && !study[authzField]))
+    else if (!authz) {
+      accessible = AccessLevel.WAITING;
+    }
+    // ln 161 else condition
+    else {
+      // ln 176
+      if (userHasAccessToResource) {
+        //ln 177
+        // if (supportedValues?.mixed?.enabled && dataAvailabilityField && study[dataAvailabilityField] === 'mixed_availability')
+        if (data_availability && data_availability === 'mixed_availability') {
+          accessible = AccessLevel.MIXED;
+        }
+        // ln 181 else
+        else {
+          accessible = AccessLevel.ACCESSIBLE;
+        }
+      }
+      // ln 184 else if
+      // isAuthorized === false
+      else if (!userHasAccessToResource) {
+        accessible = AccessLevel.UNACCESSIBLE;
+      }
+    }
+
+    return accessible;
+    /**
+     * RESULTS OF PREVIOUS APPROACH
+     * Should be -  Waiting: 1018 * Implementation: 1018
+     * Should be - Available: 109 * Implementation: 96
+     * Should be - Request Access: 45 * Implementation:40
+     * Should be - Not Available: 27 * Implementation:27
+     */
+
     /*
-    Not Available
-    Gen3_discovery.data_availability: field exists and has the value not_available (regardless of authz contents)
-    */
+    //Not Available
+    //Gen3_discovery.data_availability: field exists and has the value not_available (regardless of authz contents)
     if (data_availability && data_availability === 'not_available') {
       return AccessLevel.NOT_AVAILABLE;
     } else if (
-      /*
-    Request Access
-    gen3_discovery.authz = has a value (/programs/open, for example)
-    AND the user DOES NOT have access to that resource OR
-    Gen3_discovery.data_availability: field exists and has the value unaccessible
-    (for eg, https://healdata.org/mds/metadata/HDP00054) (regardless of authz contents)
-    */
+      // Request Access
+      // gen3_discovery.authz = has a value (/programs/open, for example)
+      // AND the user DOES NOT have access to that resource OR
+      // Gen3_discovery.data_availability: field exists and has the value unaccessible
+      // (for eg, https://healdata.org/mds/metadata/HDP00054) (regardless of authz contents)
       (authz && !userHasAccessToResource) ||
       (data_availability && data_availability === 'unaccessible')
     ) {
       return AccessLevel.UNACCESSIBLE;
+    } else if (!authz) {
+      // Waiting
+      // gen3_discovery.authz: Field is empty
+      // AND
+      // gen3_discovery.data_availability:
+      // Field does not exist OR is empty OR does not have the values described below to trigger other statuses
+      return AccessLevel.WAITING;
     } else if (authz && userHasAccessToResource && !data_availability) {
-      /**
-    Request Access
-    gen3_discovery.authz = has a value (/programs/open, for example)
-    AND the user has access to that resource
-    AND gen3_discovery.data_availability: Field does not exist OR is empty
-    OR does not have the values described above to trigger other statuses
-    */
+      // Request Access
+      // gen3_discovery.authz = has a value (/programs/open, for example)
+      // AND the user has access to that resource
+      // AND gen3_discovery.data_availability: Field does not exist OR is empty
+      // OR does not have the values described above to trigger other statuses
       return AccessLevel.ACCESSIBLE;
     } else if (
-      /*
-      Mixed Availability(New)
-      Study meets the conditions for being identified as “available”
-      AND
-      Gen3_discovery.data_availability: field exists and has the value mixed_availability
-      */
+      // Mixed Availability(New)
+      // Study meets the conditions for being identified as “available”
+      // AND
+      // Gen3_discovery.data_availability: field exists and has the value mixed_availability
       authz &&
       userHasAccessToResource &&
       data_availability === 'mixed_availability'
     ) {
       return AccessLevel.MIXED;
-    } else {
-      /* Waiting (Default) */
-      return AccessLevel.WAITING;
     }
+    // else {
+    //   // Waiting (Default)
+    //   return AccessLevel.WAITING;
+    // }
+    else return AccessLevel.OTHER;
+    */
   };
 
   return data.map((obj) => {
