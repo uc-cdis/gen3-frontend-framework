5c13fadde2ad3ba50c3b9b34fe57d8f436f3bc89
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-07-01 09:41:56 -0500

    feat(studyRegistrationRequestAccess):cleaned up studyDetails

--- packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
+++ packages/frontend/src/features/Study/StudyDetails/StudyDetails.tsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'; // Or 'next/navigation' if using App Router
+import { useRouter } from 'next/router';
 import { Drawer } from '@mantine/core';
 import StudyDetailsPanel from './StudyDetailsPanel';
 import { useDisclosure } from '@mantine/hooks';
 import SinglePageStudyDetailsPanel from './SinglePageStudyDetailsPanel';
 import { useStudyContext } from '../StudyProvider';
-import { StudyDetailView, StudyPageConfig } from '../types';
-import { DataAuthorization } from '../../../utils';
 import StudyDetailsHeaderButtons from './StudyDetailsHeaderButtons';
 import { toString } from 'lodash';
 import { useDiscoveryContext } from '../../Discovery/DiscoveryProvider';
-import { CoreState, selectUserAuthStatus, useCoreSelector } from '@gen3/core';
 
 const StudyDetails = () => {
   const { discoveryConfig: config } = useDiscoveryContext();
@@ -51,10 +48,6 @@ const StudyDetails = () => {
       open();
     }
   }, [studyDetails, open]);
-
-  const userStatus = useCoreSelector((state: CoreState) =>
-    selectUserAuthStatus(state),
-  );
 
   return (
     <Drawer.Root opened={opened} onClose={close} size="50%" position="right">
