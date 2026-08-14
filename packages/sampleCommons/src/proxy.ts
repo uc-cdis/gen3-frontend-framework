608ea8619eaa8433ffbee87e8ef2bf3f0baefa4d
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-08-14 14:36:26 -0500

    feat(studyRegistrationForm): Reverted inadvertant changes to proxy.ts

--- packages/sampleCommons/src/proxy.ts
+++ packages/sampleCommons/src/proxy.ts
@@ -4,8 +4,21 @@ export { proxy } from './proxy-impl';
 
 export const config = {
   matcher: [
-  "/DataLibrary",
-  "/Workspace",
-  "/Profile"
-],
+    '/AISearch',
+    '/Analysis',
+    '/staticNotebook/(.*)',
+    '/Crosswalk',
+    '/DataConnections',
+    '/DataDictionary',
+    '/DataLibrary',
+    '/Explorer',
+    '/notebook',
+    '/Profile',
+    '/Query',
+    '/Submission',
+    '/Workspace',
+    '/Workspaces',
+    '/app/CohortDiscovery',
+    '/app/MetadataDictionary',
+  ],
 };
