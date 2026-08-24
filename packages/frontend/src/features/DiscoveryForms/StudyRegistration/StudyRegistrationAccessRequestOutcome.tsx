f0c5dec34f07743b6ac887ed60e08922c1f66cd3
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-08-24 14:39:35 -0500

    feat(studyRegistration): Fixed error UI

--- packages/frontend/src/features/DiscoveryForms/StudyRegistration/StudyRegistrationAccessRequestOutcome.tsx
+++ packages/frontend/src/features/DiscoveryForms/StudyRegistration/StudyRegistrationAccessRequestOutcome.tsx
@@ -11,25 +11,27 @@ const StudyRegistrationAccessRequestOutcome = ({
   outcomeConfig: studyRegistrationAccessRequestFormOutcomeProps;
 }) => {
   const { content, buttons } = outcomeConfig;
+
   return (
     <div className="w-full max-w-xl m-auto text-center">
       {content?.map((item: TextContentProps, index: number) => (
         <TextContent key={index} {...item} />
       ))}
-      {buttons.map((button, index) => (
-        <Button
-          key={index}
-          component="a"
-          variant={button.variant}
-          href={button.href}
-          rightSection={button.externalLink && <IconExternalLink />}
-          target={button.externalLink ? '_blank' : undefined}
-          rel={button.externalLink ? 'noopener noreferrer' : undefined}
-          className="mt-10 mr-1"
-        >
-          {button.text}
-        </Button>
-      ))}
+      {buttons &&
+        buttons.map((button, index) => (
+          <Button
+            key={index}
+            component="a"
+            variant={button.variant}
+            href={button.href}
+            rightSection={button.externalLink && <IconExternalLink />}
+            target={button.externalLink ? '_blank' : undefined}
+            rel={button.externalLink ? 'noopener noreferrer' : undefined}
+            className="mt-10 mr-1"
+          >
+            {button.text}
+          </Button>
+        ))}
     </div>
   );
 };
