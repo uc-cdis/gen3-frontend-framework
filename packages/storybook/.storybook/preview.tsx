e5053a3b7dfbb12fd0e20f08030de0140f18cd7f
craigrbarnes <craig.barnes@intelligentlight.com>
2026-09-23 10:43:36 -0500

    Update mock user response to return 401 Unauthorized in Storybook.

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -33,55 +33,7 @@ initialize({}, [
     return HttpResponse.json({});
   }),
   http.get(`${GEN3_FENCE_API}/user`, () => {
-    return HttpResponse.json(
-      {
-        active: true,
-        authz: {
-          '/open': [
-            {
-              method: 'read',
-              service: 'fence',
-            },
-            {
-              method: 'read-storage',
-              service: 'fence',
-            },
-            {
-              method: 'read',
-              service: 'guppy',
-            },
-            {
-              method: 'read',
-              service: 'peregrine',
-            },
-            {
-              method: 'read',
-              service: 'sheepdog',
-            },
-          ],
-        },
-        azp: null,
-        certificates_uploaded: [],
-        display_name: null,
-        email: 'storybook@uchicago.edu',
-        groups: [],
-        idp: 'storybook',
-        is_admin: false,
-        message: '',
-        name: 'storybook@uchicago.edu',
-        phone_number: null,
-        preferred_username: null,
-        primary_google_service_account: null,
-        project_access: {},
-        resources: ['/open'],
-        resources_granted: [],
-        role: 'user',
-        sub: '374',
-        user_id: 374,
-        username: 'storybook@uchicago.edu',
-      },
-      { status: 200 },
-    );
+    return HttpResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }),
 ]);
 
